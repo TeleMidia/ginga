0cc42bf67b017179bcf5baeebdfaff0df21e918c
Rafael Savignon Marinho <savignon@telemidia.puc-rio.br>
2010-11-10 18:49:34 -0200

    gingacc-ic Windows port code added.

--- gingacc-ic/include/ic/curlic/CurlInteractiveChannel.h
+++ gingacc-ic/include/ic/curlic/CurlInteractiveChannel.h
@@ -52,6 +52,12 @@ http://www.telemidia.puc-rio.br
 
 #include "../IInteractiveChannel.h"
 
+#ifdef _WIN32
+#include <io.h>
+#include <pthread.h>
+#define O_LARGEFILE 0 
+#endif
+
 #include "curl/curl.h"
 
 #include <sys/stat.h>
