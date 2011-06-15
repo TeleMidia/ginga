e490ca92ac3d011a9245bedaafbb7bc7dfb558bc
Marcio Ferreira Moreno <marcio@telemidia.puc-rio.br>
2011-06-15 15:33:24 -0300

    supporting transparency for embedded ncl objects

--- gingacc-system/include/system/io/interface/device/dfb/DFBDeviceScreen.h
+++ gingacc-system/include/system/io/interface/device/dfb/DFBDeviceScreen.h
@@ -67,7 +67,6 @@ extern "C" {
 	DFBResult err = x;                                            \
 	if (err != DFB_OK) {                                          \
 		fprintf( stderr, "%s <%d>:\n\t", __FILE__, __LINE__ );    \
-		DirectFBErrorFatal( #x, err );                            \
 	}                                                             \
 }
 #endif /*DFBCHECK*/
