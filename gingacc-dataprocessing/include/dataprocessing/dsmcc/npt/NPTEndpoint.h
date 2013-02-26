45f35dd2d7614d7cc433330d740d557d7db61d6d
fnagato <fnagato@telemidia.puc-rio.br>
2013-02-26 11:34:09 -0300

    adding missed files

---
+++ gingacc-dataprocessing/include/dataprocessing/dsmcc/npt/NPTEndpoint.h
@@ -0,0 +1,99 @@
+/******************************************************************************
+Este arquivo eh parte da implementacao do ambiente declarativo do middleware
+Ginga (Ginga-NCL).
+
+Direitos Autorais Reservados (c) 1989-2007 PUC-Rio/Laboratorio TeleMidia
+
+Este programa eh software livre; voce pode redistribui-lo e/ou modificah-lo sob
+os termos da Licenca Publica Geral GNU versao 2 conforme publicada pela Free
+Software Foundation.
+
+Este programa eh distribuido na expectativa de que seja util, porem, SEM
+NENHUMA GARANTIA; nem mesmo a garantia implicita de COMERCIABILIDADE OU
+ADEQUACAO A UMA FINALIDADE ESPECIFICA. Consulte a Licenca Publica Geral do
+GNU versao 2 para mais detalhes.
+
+Voce deve ter recebido uma copia da Licenca Publica Geral do GNU versao 2 junto
+com este programa; se nao, escreva para a Free Software Foundation, Inc., no
+endereco 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+
+Para maiores informacoes:
+ncl @ telemidia.puc-rio.br
+http://www.ncl.org.br
+http://www.ginga.org.br
+http://www.telemidia.puc-rio.br
+******************************************************************************
+This file is part of the declarative environment of middleware Ginga (Ginga-NCL)
+
+Copyright: 1989-2007 PUC-RIO/LABORATORIO TELEMIDIA, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 2 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License version 2 for more
+details.
+
+You should have received a copy of the GNU General Public License version 2
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+For further information contact:
+ncl @ telemidia.puc-rio.br
+http://www.ncl.org.br
+http://www.ginga.org.br
+http://www.telemidia.puc-rio.br
+*******************************************************************************/
+
+#ifndef NPTENDPOINT_H_
+#define NPTENDPOINT_H_
+
+#include <stdint.h>
+#include "MpegDescriptor.h"
+
+using namespace std;
+
+namespace br {
+namespace pucrio {
+namespace telemidia {
+namespace ginga {
+namespace core {
+namespace dataprocessing {
+namespace dsmcc {
+namespace npt {
+
+class NPTEndpoint : public MpegDescriptor {
+	private:
+
+	protected:
+		uint64_t startNPT;
+		uint64_t stopNPT;
+
+		virtual int process();
+		int updateStream();
+
+		unsigned int calculateDescriptorSize();
+
+	public:
+		NPTEndpoint();
+		~NPTEndpoint();
+
+		uint64_t getStartNPT();
+		uint64_t getStopNPT();
+		void setStartNPT(uint64_t npt);
+		void setStopNPT(uint64_t npt);
+
+};
+
+}
+}
+}
+}
+}
+}
+}
+}
+
+#endif /* NPTENDPOINT_H_ */
