bbedf2d005632b516a6591d1be652af624c36715
Rodrigo Costa <rodrigocosta@git.telemidia.puc-rio.br>
2014-07-09 13:37:45 -0300

    Adding Windows batch scripts to fetch and copy deps. Fixing a minor bug on windows build.

---
+++ contrib/fetch_deps.bat
@@ -0,0 +1,15 @@
+::This is a batch script that fetches the deps
+::from telemidiatc1 for build Ginga on Windows.
+@echo off
+
+set gingaVer=0.13.6
+set contribVer=1
+set archive=contrib_v%gingaVer%.%contribVer%.zip
+
+set remoteHost=\\telemidiatc1
+set remotePath=TeleMidia\deps\ginga\windows
+
+echo Copying %remoteHost%\%remotePath%\%archive% to current directory.
+copy /Z /-Y %remoteHost%\%remotePath%\%archive% .
+
+pause
