71e4092a6709f63ef82afcbfcba75d9d0517131c
marcio <marcio@telemidia.puc-rio.br>
2014-11-10 15:26:56 -0300

    preparing 0.13.6 release

--- gingacc-player/src/image/ImagePlayer.cpp
+++ gingacc-player/src/image/ImagePlayer.cpp
@@ -86,7 +86,7 @@ namespace player {
 			IInteractiveChannel* ic = icm->createInteractiveChannel(mrl);
 			newMrl = itos((long int)this);
 			ic->setSourceTarget(newMrl);
-			ic->reserveUrl(mrl, NULL, "GingaNCL/0.13.5");
+			ic->reserveUrl(mrl, NULL, "GingaNCL/0.13.6");
 			ic->performUrl();
 
 			icm->releaseInteractiveChannel(ic);
