d7ce8728b7d2565bb86c3d60a8b6553b3f34f629
Rafael Diniz <rafaeldiniz@telemidia.puc-rio.br>
2015-05-11 16:11:52 -0300

    SVG support Added

---
+++ gingacc-mb/src/interface/sdl/content/image/SDLSvgDecoder.cpp
@@ -0,0 +1,125 @@
+/******************************************************************************
+Este arquivo eh parte da implementacao do ambiente declarativo do middleware
+Ginga (Ginga-NCL).
+
+Direitos Autorais Reservados (c) 1989-2007 PUC-Rio/Laboratorio TeleMidia
+
+Este programa eh software livre; voce pode redistribui-lo e/ou modificah-lo sob
+os termos da Licenca Publica Geral GNU versao 2 conforme publicada pela Free
+Software Foundation.
+
+Este programa eh distribuido na expectativa de que seja util, porem, SEM
+NENHUMA GARANTIA; nem mesmo a garantia implicita de COMERCIABILIDADE OU
+ADEQUACAO A UMA FINALIDADE ESPECIFICA. Consulte a Licenca Publica Geral do
+GNU versao 2 para mais detalhes.
+
+Voce deve ter recebido uma copia da Licenca Publica Geral do GNU versao 2 junto
+com este programa; se nao, escreva para a Free Software Foundation, Inc., no
+endereco 59 Temple Street, Suite 330, Boston, MA 02111-1307 USA.
+
+Para maiores informacoes:
+ncl @ telemidia.puc-rio.br
+http://www.ncl.org.br
+http://www.ginga.org.br
+http://www.telemidia.puc-rio.br
+******************************************************************************
+This file is part of the declarative environment of middleware Ginga (Ginga-NCL)
+
+Copyright: 1989-2015 PUC-RIO/LABORATORIO TELEMIDIA, All Rights Reserved.
+
+This program is free software; you can redistribute it and/or modify it under
+the terms of the GNU General Public License version 2 as published by
+the Free Software Foundation.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+PARTICULAR PURPOSE.  See the GNU General Public License version 2 for more
+details.
+
+You should have received a copy of the GNU General Public License version 2
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+
+For further information contact:
+ncl @ telemidia.puc-rio.br
+http://www.ncl.org.br
+http://www.ginga.org.br
+http://www.telemidia.puc-rio.br
+*******************************************************************************/
+
+#include <librsvg/rsvg.h>
+#include <cairo.h>
+
+#include "mb/interface/sdl/content/image/SDLSvgDecoder.h"
+#include "config.h"
+
+
+using namespace std;
+
+
+namespace br {
+namespace pucrio {
+namespace telemidia {
+namespace ginga {
+namespace core {
+namespace mb {
+    
+    SDLSvgDecoder::SDLSvgDecoder(string filename) {
+
+        filePath.assign(filename);
+        
+    }
+
+    SDLSvgDecoder::~SDLSvgDecoder() {
+
+    }
+
+    SDL_Surface *SDLSvgDecoder::decode(int width, int height) {
+        RsvgHandle* h;
+        RsvgDimensionData dim;
+        GError* e = NULL;
+        cairo_surface_t *cairoSurface;
+        cairo_t *cairoState;
+
+        /* RSVG initiation */
+        h = rsvg_handle_new_from_file(filePath.c_str(), &e);
+
+        rsvg_handle_get_dimensions (h, &dim);
+        int x = dim.width;
+        int y = dim.height;
+        int stride = x * 4; // ARGB
+
+        /* Cairo Initiation */
+        uint8_t *image = (uint8_t *) malloc (stride * y); // ARGB uses 4 bytes / pixel
+
+        // here the correct would be...
+        // cairoSurface = cairo_image_surface_create_for_data (image, CAIRO_FORMAT_ARGB32, width, height, width * 4);
+        cairoSurface = cairo_image_surface_create_for_data (image, CAIRO_FORMAT_ARGB32, x, y, stride);
+        cairoState = cairo_create (cairoSurface);
+
+        rsvg_handle_render_cairo (h, cairoState);
+
+//        cairo_surface_write_to_png (cairoSurface, "/tmp/out.png");
+
+        // Match ARGB32 format masks
+        Uint32 amask = 0xff000000;
+        Uint32 rmask = 0x00ff0000;
+        Uint32 gmask = 0x0000ff00;
+        Uint32 bmask = 0x000000ff;
+
+        SDL_Surface *sdlSurface = SDL_CreateRGBSurfaceFrom ( (void *) image, x, y, 32, stride, rmask, gmask, bmask, amask);
+
+        cairo_surface_destroy (cairoSurface);
+        cairo_destroy (cairoState);
+
+        return sdlSurface;
+    }
+
+
+}
+}
+}
+}
+}
+}
+
