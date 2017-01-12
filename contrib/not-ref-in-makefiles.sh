f5639feb64164c3cf7be9a3e9a1c965e6fcee7c3
Guilherme F. Lima <gflima@sdf.org>
2017-01-12 18:18:41 -0200

    maint: Remove unused headers

--- contrib/not-ref-in-makefiles.sh
+++
@@ -1,9 +0,0 @@
-for src in `find . -name '*.cpp'`; do
-  found=false
-  for mk in `find . -name 'Makefile.am'`; do
-    if grep -q  "`basename $src`" "$mk"; then
-      found=true
-    fi
-  done
-  ! $found && echo "$src"
-done
--- maint.mk
+++ maint.mk
@@ -10,3 +10,21 @@ bootstrap:
 etags:
 	find . -type f \( -name '*.cpp' -or -name '*.h' \) -print\
 	  | etags - --declarations
+
+.PHONY: list-unused-sources
+list-unused-sources:
+	@out="./$@.tmp";\
+	cat `find . -name 'Makefile.am'` >"$$out";\
+	for src in `find . -name '*.cpp'`; do\
+	  egrep -q  "\<`basename $$src`\>" "$$out";\
+	done;\
+	rm -f "$$out";
+
+.PHONY: list-unused-headers
+list-unused-headers:
+	@out="./$@.tmp";\
+	cat `find . \( -name '*.cpp' -or -name '*.h' \) ` >"$$out";\
+	for h in `find . -name '*.h'`; do\
+	  fgrep -q  "`basename $$h`" "$$out" || echo "$$h";\
+	done;\
+	rm -f "$$out";
