a624048f8971ae1d06855e3007dd68576d6ab961
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2017-01-31 17:22:47 +0100

    src/mb: C++11 map initialization
    * src/mb/CodeMap.cpp: Likewise.
    * src/mb/CodeMap.h: Likewise.
    * src/mb/Display.cpp: Likewise.

--- src/mb/CodeMap.cpp
+++ src/mb/CodeMap.cpp
@@ -20,259 +20,123 @@ along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 
 GINGA_MB_BEGIN
 
-const int CodeMap::KEY_QUIT = -1;
-const int CodeMap::KEY_NULL = 1;
-const int CodeMap::KEY_0 = 2;
-const int CodeMap::KEY_1 = 3;
-const int CodeMap::KEY_2 = 4;
-const int CodeMap::KEY_3 = 5;
-const int CodeMap::KEY_4 = 6;
-const int CodeMap::KEY_5 = 7;
-const int CodeMap::KEY_6 = 8;
-const int CodeMap::KEY_7 = 9;
-const int CodeMap::KEY_8 = 10;
-const int CodeMap::KEY_9 = 11;
-
-const int CodeMap::KEY_SMALL_A = 12;
-const int CodeMap::KEY_SMALL_B = 13;
-const int CodeMap::KEY_SMALL_C = 14;
-const int CodeMap::KEY_SMALL_D = 15;
-const int CodeMap::KEY_SMALL_E = 16;
-const int CodeMap::KEY_SMALL_F = 17;
-const int CodeMap::KEY_SMALL_G = 18;
-const int CodeMap::KEY_SMALL_H = 19;
-const int CodeMap::KEY_SMALL_I = 20;
-const int CodeMap::KEY_SMALL_J = 21;
-const int CodeMap::KEY_SMALL_K = 22;
-const int CodeMap::KEY_SMALL_L = 23;
-const int CodeMap::KEY_SMALL_M = 24;
-const int CodeMap::KEY_SMALL_N = 25;
-const int CodeMap::KEY_SMALL_O = 26;
-const int CodeMap::KEY_SMALL_P = 27;
-const int CodeMap::KEY_SMALL_Q = 28;
-const int CodeMap::KEY_SMALL_R = 29;
-const int CodeMap::KEY_SMALL_S = 30;
-const int CodeMap::KEY_SMALL_T = 31;
-const int CodeMap::KEY_SMALL_U = 32;
-const int CodeMap::KEY_SMALL_V = 33;
-const int CodeMap::KEY_SMALL_W = 34;
-const int CodeMap::KEY_SMALL_X = 35;
-const int CodeMap::KEY_SMALL_Y = 36;
-const int CodeMap::KEY_SMALL_Z = 37;
-
-const int CodeMap::KEY_CAPITAL_A = 38;
-const int CodeMap::KEY_CAPITAL_B = 39;
-const int CodeMap::KEY_CAPITAL_C = 40;
-const int CodeMap::KEY_CAPITAL_D = 41;
-const int CodeMap::KEY_CAPITAL_E = 42;
-const int CodeMap::KEY_CAPITAL_F = 43;
-const int CodeMap::KEY_CAPITAL_G = 44;
-const int CodeMap::KEY_CAPITAL_H = 45;
-const int CodeMap::KEY_CAPITAL_I = 46;
-const int CodeMap::KEY_CAPITAL_J = 47;
-const int CodeMap::KEY_CAPITAL_K = 48;
-const int CodeMap::KEY_CAPITAL_L = 49;
-const int CodeMap::KEY_CAPITAL_M = 50;
-const int CodeMap::KEY_CAPITAL_N = 51;
-const int CodeMap::KEY_CAPITAL_O = 52;
-const int CodeMap::KEY_CAPITAL_P = 53;
-const int CodeMap::KEY_CAPITAL_Q = 54;
-const int CodeMap::KEY_CAPITAL_R = 55;
-const int CodeMap::KEY_CAPITAL_S = 56;
-const int CodeMap::KEY_CAPITAL_T = 57;
-const int CodeMap::KEY_CAPITAL_U = 58;
-const int CodeMap::KEY_CAPITAL_V = 59;
-const int CodeMap::KEY_CAPITAL_W = 60;
-const int CodeMap::KEY_CAPITAL_X = 61;
-const int CodeMap::KEY_CAPITAL_Y = 62;
-const int CodeMap::KEY_CAPITAL_Z = 63;
-
-const int CodeMap::KEY_PAGE_DOWN = 64;
-const int CodeMap::KEY_PAGE_UP = 65;
-
-const int CodeMap::KEY_F1 = 66;
-const int CodeMap::KEY_F2 = 67;
-const int CodeMap::KEY_F3 = 68;
-const int CodeMap::KEY_F4 = 69;
-const int CodeMap::KEY_F5 = 70;
-const int CodeMap::KEY_F6 = 71;
-const int CodeMap::KEY_F7 = 72;
-const int CodeMap::KEY_F8 = 73;
-const int CodeMap::KEY_F9 = 74;
-const int CodeMap::KEY_F10 = 75;
-const int CodeMap::KEY_F11 = 76;
-const int CodeMap::KEY_F12 = 77;
-
-const int CodeMap::KEY_PLUS_SIGN = 78;
-const int CodeMap::KEY_MINUS_SIGN = 79;
-
-const int CodeMap::KEY_ASTERISK = 80;
-const int CodeMap::KEY_NUMBER_SIGN = 81;
-
-const int CodeMap::KEY_PERIOD = 82;
-
-const int CodeMap::KEY_SUPER = 83;
-const int CodeMap::KEY_PRINTSCREEN = 84;
-const int CodeMap::KEY_MENU = 85;
-const int CodeMap::KEY_INFO = 86;
-const int CodeMap::KEY_EPG = 87;
-
-const int CodeMap::KEY_CURSOR_DOWN = 88;
-const int CodeMap::KEY_CURSOR_LEFT = 89;
-const int CodeMap::KEY_CURSOR_RIGHT = 90;
-const int CodeMap::KEY_CURSOR_UP = 91;
-
-const int CodeMap::KEY_CHANNEL_DOWN = 92;
-const int CodeMap::KEY_CHANNEL_UP = 93;
-
-const int CodeMap::KEY_VOLUME_DOWN = 94;
-const int CodeMap::KEY_VOLUME_UP = 95;
-
-const int CodeMap::KEY_ENTER = 96;
-
-const int CodeMap::KEY_RED = 98;
-const int CodeMap::KEY_GREEN = 99;
-const int CodeMap::KEY_YELLOW = 100;
-const int CodeMap::KEY_BLUE = 101;
-
-const int CodeMap::KEY_SPACE = 102;
-const int CodeMap::KEY_BACKSPACE = 103;
-const int CodeMap::KEY_BACK = 104;
-const int CodeMap::KEY_ESCAPE = 105;
-const int CodeMap::KEY_EXIT = 106;
-
-const int CodeMap::KEY_POWER = 107;
-const int CodeMap::KEY_REWIND = 108;
-const int CodeMap::KEY_STOP = 109;
-const int CodeMap::KEY_EJECT = 110;
-const int CodeMap::KEY_PLAY = 111;
-const int CodeMap::KEY_RECORD = 112;
-const int CodeMap::KEY_PAUSE = 113;
-
-const int CodeMap::KEY_GREATER_THAN_SIGN = 114;
-const int CodeMap::KEY_LESS_THAN_SIGN = 115;
-
-const int CodeMap::KEY_TAB = 116;
-const int CodeMap::KEY_TAP = 117;
-
 CodeMap *CodeMap::_instance = NULL;
+
+map <string, int> CodeMap::keyMap = {
+  {"QUIT", KEY_QUIT},
+  {"NO_CODE", KEY_NULL},
+  {"0", KEY_0},
+  {"1", KEY_1},
+  {"2", KEY_2},
+  {"3", KEY_3},
+  {"4", KEY_4},
+  {"5", KEY_5},
+  {"6", KEY_6},
+  {"7", KEY_7},
+  {"8", KEY_8},
+  {"9", KEY_9},
+
+  {"a", KEY_SMALL_A},
+  {"b", KEY_SMALL_B},
+  {"c", KEY_SMALL_C},
+  {"d", KEY_SMALL_D},
+  {"e", KEY_SMALL_E},
+  {"f", KEY_SMALL_F},
+  {"g", KEY_SMALL_G},
+  {"h", KEY_SMALL_H},
+  {"i", KEY_SMALL_I},
+  {"j", KEY_SMALL_J},
+  {"k", KEY_SMALL_K},
+  {"l", KEY_SMALL_L},
+  {"m", KEY_SMALL_M},
+  {"n", KEY_SMALL_N},
+  {"o", KEY_SMALL_O},
+  {"p", KEY_SMALL_P},
+  {"q", KEY_SMALL_Q},
+  {"r", KEY_SMALL_R},
+  {"s", KEY_SMALL_S},
+  {"t", KEY_SMALL_T},
+  {"u", KEY_SMALL_U},
+  {"v", KEY_SMALL_V},
+  {"w", KEY_SMALL_W},
+  {"x", KEY_SMALL_X},
+  {"y", KEY_SMALL_Y},
+  {"z", KEY_SMALL_Z},
+  {"A", KEY_CAPITAL_A},
+  {"B", KEY_CAPITAL_B},
+  {"C", KEY_CAPITAL_C},
+  {"D", KEY_CAPITAL_D},
+  {"E", KEY_CAPITAL_E},
+  {"F", KEY_CAPITAL_F},
+  {"G", KEY_CAPITAL_G},
+  {"H", KEY_CAPITAL_H},
+  {"I", KEY_CAPITAL_I},
+  {"J", KEY_CAPITAL_J},
+  {"K", KEY_CAPITAL_K},
+  {"L", KEY_CAPITAL_L},
+  {"M", KEY_CAPITAL_M},
+  {"N", KEY_CAPITAL_N},
+  {"O", KEY_CAPITAL_O},
+  {"P", KEY_CAPITAL_P},
+  {"Q", KEY_CAPITAL_Q},
+  {"R", KEY_CAPITAL_R},
+  {"S", KEY_CAPITAL_S},
+  {"T", KEY_CAPITAL_T},
+  {"U", KEY_CAPITAL_U},
+  {"V", KEY_CAPITAL_V},
+  {"W", KEY_CAPITAL_W},
+  {"X", KEY_CAPITAL_X},
+  {"Y", KEY_CAPITAL_Y},
+  {"Z", KEY_CAPITAL_Z},
+
+  {"*", KEY_ASTERISK},
+  {"#", KEY_NUMBER_SIGN},
+  {".", KEY_PERIOD},
+
+  {"MENU", KEY_MENU},
+  {"INFO", KEY_INFO},
+  {"GUIDE", KEY_EPG},
+
+  {"CURSOR_DOWN", KEY_CURSOR_DOWN},
+  {"CURSOR_LEFT", KEY_CURSOR_LEFT},
+  {"CURSOR_RIGHT", KEY_CURSOR_RIGHT},
+  {"CURSOR_UP", KEY_CURSOR_UP},
+
+  {"CHANNEL_DOWN", KEY_CHANNEL_DOWN},
+  {"CHANNEL_UP", KEY_CHANNEL_UP},
+
+  {"VOLUME_DOWN", KEY_VOLUME_DOWN},
+  {"VOLUME_UP", KEY_VOLUME_UP},
+
+  {"ENTER", KEY_ENTER},
+  {"TAP", KEY_TAP},
+
+  {"RED", KEY_RED},
+  {"GREEN", KEY_GREEN},
+  {"YELLOW", KEY_YELLOW},
+  {"BLUE", KEY_BLUE},
+
+  {"	", KEY_TAB},
+  {" ", KEY_SPACE},
+  {"BACK", KEY_BACK},
+  {"EXIT", KEY_EXIT},
+
+  {"POWER", KEY_POWER},
+  {"REWIND", KEY_REWIND},
+  {"STOP", KEY_STOP},
+  {"EJECT", KEY_EJECT},
+  {"PLAY", KEY_PLAY},
+  {"RECORD", KEY_RECORD},
+  {"PAUSE", KEY_PAUSE}
+};
 
 CodeMap::CodeMap ()
 {
-  map<string, int>::iterator i;
-
-  keyMap["QUIT"] = KEY_QUIT;
-  keyMap["NO_CODE"] = KEY_NULL;
-  keyMap["0"] = KEY_0;
-  keyMap["1"] = KEY_1;
-  keyMap["2"] = KEY_2;
-  keyMap["3"] = KEY_3;
-  keyMap["4"] = KEY_4;
-  keyMap["5"] = KEY_5;
-  keyMap["6"] = KEY_6;
-  keyMap["7"] = KEY_7;
-  keyMap["8"] = KEY_8;
-  keyMap["9"] = KEY_9;
-
-  keyMap["a"] = KEY_SMALL_A;
-  keyMap["b"] = KEY_SMALL_B;
-  keyMap["c"] = KEY_SMALL_C;
-  keyMap["d"] = KEY_SMALL_D;
-  keyMap["e"] = KEY_SMALL_E;
-  keyMap["f"] = KEY_SMALL_F;
-  keyMap["g"] = KEY_SMALL_G;
-  keyMap["h"] = KEY_SMALL_H;
-  keyMap["i"] = KEY_SMALL_I;
-  keyMap["j"] = KEY_SMALL_J;
-  keyMap["k"] = KEY_SMALL_K;
-  keyMap["l"] = KEY_SMALL_L;
-  keyMap["m"] = KEY_SMALL_M;
-  keyMap["n"] = KEY_SMALL_N;
-  keyMap["o"] = KEY_SMALL_O;
-  keyMap["p"] = KEY_SMALL_P;
-  keyMap["q"] = KEY_SMALL_Q;
-  keyMap["r"] = KEY_SMALL_R;
-  keyMap["s"] = KEY_SMALL_S;
-  keyMap["t"] = KEY_SMALL_T;
-  keyMap["u"] = KEY_SMALL_U;
-  keyMap["v"] = KEY_SMALL_V;
-  keyMap["w"] = KEY_SMALL_W;
-  keyMap["x"] = KEY_SMALL_X;
-  keyMap["y"] = KEY_SMALL_Y;
-  keyMap["z"] = KEY_SMALL_Z;
-
-  keyMap["A"] = KEY_CAPITAL_A;
-  keyMap["B"] = KEY_CAPITAL_B;
-  keyMap["C"] = KEY_CAPITAL_C;
-  keyMap["D"] = KEY_CAPITAL_D;
-  keyMap["E"] = KEY_CAPITAL_E;
-  keyMap["F"] = KEY_CAPITAL_F;
-  keyMap["G"] = KEY_CAPITAL_G;
-  keyMap["H"] = KEY_CAPITAL_H;
-  keyMap["I"] = KEY_CAPITAL_I;
-  keyMap["J"] = KEY_CAPITAL_J;
-  keyMap["K"] = KEY_CAPITAL_K;
-  keyMap["L"] = KEY_CAPITAL_L;
-  keyMap["M"] = KEY_CAPITAL_M;
-  keyMap["N"] = KEY_CAPITAL_N;
-  keyMap["O"] = KEY_CAPITAL_O;
-  keyMap["P"] = KEY_CAPITAL_P;
-  keyMap["Q"] = KEY_CAPITAL_Q;
-  keyMap["R"] = KEY_CAPITAL_R;
-  keyMap["S"] = KEY_CAPITAL_S;
-  keyMap["T"] = KEY_CAPITAL_T;
-  keyMap["U"] = KEY_CAPITAL_U;
-  keyMap["V"] = KEY_CAPITAL_V;
-  keyMap["W"] = KEY_CAPITAL_W;
-  keyMap["X"] = KEY_CAPITAL_X;
-  keyMap["Y"] = KEY_CAPITAL_Y;
-  keyMap["Z"] = KEY_CAPITAL_Z;
-
-  keyMap["*"] = KEY_ASTERISK;
-  keyMap["#"] = KEY_NUMBER_SIGN;
-
-  keyMap["."] = KEY_PERIOD;
-
-  keyMap["MENU"] = KEY_MENU;
-  keyMap["INFO"] = KEY_INFO;
-  keyMap["GUIDE"] = KEY_EPG;
-
-  keyMap["CURSOR_DOWN"] = KEY_CURSOR_DOWN;
-  keyMap["CURSOR_LEFT"] = KEY_CURSOR_LEFT;
-  keyMap["CURSOR_RIGHT"] = KEY_CURSOR_RIGHT;
-  keyMap["CURSOR_UP"] = KEY_CURSOR_UP;
-
-  keyMap["CHANNEL_DOWN"] = KEY_CHANNEL_DOWN;
-  keyMap["CHANNEL_UP"] = KEY_CHANNEL_UP;
-
-  keyMap["VOLUME_DOWN"] = KEY_VOLUME_DOWN;
-  keyMap["VOLUME_UP"] = KEY_VOLUME_UP;
-
-  keyMap["ENTER"] = KEY_ENTER;
-  keyMap["TAP"] = KEY_TAP;
-
-  keyMap["RED"] = KEY_RED;
-  keyMap["GREEN"] = KEY_GREEN;
-  keyMap["YELLOW"] = KEY_YELLOW;
-  keyMap["BLUE"] = KEY_BLUE;
-
-  keyMap["	"] = KEY_TAB;
-  keyMap[" "] = KEY_SPACE;
-  keyMap["BACK"] = KEY_BACK;
-  keyMap["EXIT"] = KEY_EXIT;
-
-  keyMap["POWER"] = KEY_POWER;
-  keyMap["REWIND"] = KEY_REWIND;
-  keyMap["STOP"] = KEY_STOP;
-  keyMap["EJECT"] = KEY_EJECT;
-  keyMap["PLAY"] = KEY_PLAY;
-  keyMap["RECORD"] = KEY_RECORD;
-  keyMap["PAUSE"] = KEY_PAUSE;
-
-  for (i = keyMap.begin (); i != keyMap.end (); i++)
-    {
-      valueMap[i->second] = i->first;
-    }
+  map<string, int>::iterator it;
+  for (it = keyMap.begin (); it != keyMap.end (); ++it)
+  {
+    valueMap[it->second] = it->first;
+  }
 }
 
 CodeMap *
--- src/mb/CodeMap.h
+++ src/mb/CodeMap.h
@@ -22,149 +22,153 @@ along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 
 GINGA_MB_BEGIN
 
+
 class CodeMap
 {
 public:
-  static const int KEY_QUIT;
-  static const int KEY_NULL;
-  static const int KEY_0;
-  static const int KEY_1;
-  static const int KEY_2;
-  static const int KEY_3;
-  static const int KEY_4;
-  static const int KEY_5;
-  static const int KEY_6;
-  static const int KEY_7;
-  static const int KEY_8;
-  static const int KEY_9;
+  enum CodeMap_KeyCode
+  {
+    KEY_QUIT = -1,
+    KEY_NULL = 1,
+    KEY_0,
+    KEY_1,
+    KEY_2,
+    KEY_3,
+    KEY_4,
+    KEY_5,
+    KEY_6,
+    KEY_7,
+    KEY_8,
+    KEY_9,
 
-  static const int KEY_SMALL_A;
-  static const int KEY_SMALL_B;
-  static const int KEY_SMALL_C;
-  static const int KEY_SMALL_D;
-  static const int KEY_SMALL_E;
-  static const int KEY_SMALL_F;
-  static const int KEY_SMALL_G;
-  static const int KEY_SMALL_H;
-  static const int KEY_SMALL_I;
-  static const int KEY_SMALL_J;
-  static const int KEY_SMALL_K;
-  static const int KEY_SMALL_L;
-  static const int KEY_SMALL_M;
-  static const int KEY_SMALL_N;
-  static const int KEY_SMALL_O;
-  static const int KEY_SMALL_P;
-  static const int KEY_SMALL_Q;
-  static const int KEY_SMALL_R;
-  static const int KEY_SMALL_S;
-  static const int KEY_SMALL_T;
-  static const int KEY_SMALL_U;
-  static const int KEY_SMALL_V;
-  static const int KEY_SMALL_W;
-  static const int KEY_SMALL_X;
-  static const int KEY_SMALL_Y;
-  static const int KEY_SMALL_Z;
+    KEY_SMALL_A,
+    KEY_SMALL_B,
+    KEY_SMALL_C,
+    KEY_SMALL_D,
+    KEY_SMALL_E,
+    KEY_SMALL_F,
+    KEY_SMALL_G,
+    KEY_SMALL_H,
+    KEY_SMALL_I,
+    KEY_SMALL_J,
+    KEY_SMALL_K,
+    KEY_SMALL_L,
+    KEY_SMALL_M,
+    KEY_SMALL_N,
+    KEY_SMALL_O,
+    KEY_SMALL_P,
+    KEY_SMALL_Q,
+    KEY_SMALL_R,
+    KEY_SMALL_S,
+    KEY_SMALL_T,
+    KEY_SMALL_U,
+    KEY_SMALL_V,
+    KEY_SMALL_W,
+    KEY_SMALL_X,
+    KEY_SMALL_Y,
+    KEY_SMALL_Z,
 
-  static const int KEY_CAPITAL_A;
-  static const int KEY_CAPITAL_B;
-  static const int KEY_CAPITAL_C;
-  static const int KEY_CAPITAL_D;
-  static const int KEY_CAPITAL_E;
-  static const int KEY_CAPITAL_F;
-  static const int KEY_CAPITAL_G;
-  static const int KEY_CAPITAL_H;
-  static const int KEY_CAPITAL_I;
-  static const int KEY_CAPITAL_J;
-  static const int KEY_CAPITAL_K;
-  static const int KEY_CAPITAL_L;
-  static const int KEY_CAPITAL_M;
-  static const int KEY_CAPITAL_N;
-  static const int KEY_CAPITAL_O;
-  static const int KEY_CAPITAL_P;
-  static const int KEY_CAPITAL_Q;
-  static const int KEY_CAPITAL_R;
-  static const int KEY_CAPITAL_S;
-  static const int KEY_CAPITAL_T;
-  static const int KEY_CAPITAL_U;
-  static const int KEY_CAPITAL_V;
-  static const int KEY_CAPITAL_W;
-  static const int KEY_CAPITAL_X;
-  static const int KEY_CAPITAL_Y;
-  static const int KEY_CAPITAL_Z;
+    KEY_CAPITAL_A,
+    KEY_CAPITAL_B,
+    KEY_CAPITAL_C,
+    KEY_CAPITAL_D,
+    KEY_CAPITAL_E,
+    KEY_CAPITAL_F,
+    KEY_CAPITAL_G,
+    KEY_CAPITAL_H,
+    KEY_CAPITAL_I,
+    KEY_CAPITAL_J,
+    KEY_CAPITAL_K,
+    KEY_CAPITAL_L,
+    KEY_CAPITAL_M,
+    KEY_CAPITAL_N,
+    KEY_CAPITAL_O,
+    KEY_CAPITAL_P,
+    KEY_CAPITAL_Q,
+    KEY_CAPITAL_R,
+    KEY_CAPITAL_S,
+    KEY_CAPITAL_T,
+    KEY_CAPITAL_U,
+    KEY_CAPITAL_V,
+    KEY_CAPITAL_W,
+    KEY_CAPITAL_X,
+    KEY_CAPITAL_Y,
+    KEY_CAPITAL_Z,
 
-  static const int KEY_PAGE_DOWN;
-  static const int KEY_PAGE_UP;
+    KEY_PAGE_DOWN,
+    KEY_PAGE_UP,
 
-  static const int KEY_F1;
-  static const int KEY_F2;
-  static const int KEY_F3;
-  static const int KEY_F4;
-  static const int KEY_F5;
-  static const int KEY_F6;
-  static const int KEY_F7;
-  static const int KEY_F8;
-  static const int KEY_F9;
-  static const int KEY_F10;
-  static const int KEY_F11;
-  static const int KEY_F12;
+    KEY_F1,
+    KEY_F2,
+    KEY_F3,
+    KEY_F4,
+    KEY_F5,
+    KEY_F6,
+    KEY_F7,
+    KEY_F8,
+    KEY_F9,
+    KEY_F10,
+    KEY_F11,
+    KEY_F12,
 
-  static const int KEY_PLUS_SIGN;
-  static const int KEY_MINUS_SIGN;
+    KEY_PLUS_SIGN,
+    KEY_MINUS_SIGN,
 
-  static const int KEY_ASTERISK;
-  static const int KEY_NUMBER_SIGN;
+    KEY_ASTERISK,
+    KEY_NUMBER_SIGN,
 
-  static const int KEY_PERIOD;
+    KEY_PERIOD,
 
-  static const int KEY_SUPER;
-  static const int KEY_PRINTSCREEN;
-  static const int KEY_MENU;
-  static const int KEY_INFO;
-  static const int KEY_EPG;
+    KEY_SUPER,
+    KEY_PRINTSCREEN,
+    KEY_MENU,
+    KEY_INFO,
+    KEY_EPG,
 
-  static const int KEY_CURSOR_DOWN;
-  static const int KEY_CURSOR_LEFT;
-  static const int KEY_CURSOR_RIGHT;
-  static const int KEY_CURSOR_UP;
+    KEY_CURSOR_DOWN,
+    KEY_CURSOR_LEFT,
+    KEY_CURSOR_RIGHT,
+    KEY_CURSOR_UP,
 
-  static const int KEY_CHANNEL_DOWN;
-  static const int KEY_CHANNEL_UP;
+    KEY_CHANNEL_DOWN,
+    KEY_CHANNEL_UP,
 
-  static const int KEY_VOLUME_DOWN;
-  static const int KEY_VOLUME_UP;
+    KEY_VOLUME_DOWN,
+    KEY_VOLUME_UP,
 
-  static const int KEY_ENTER;
+    KEY_ENTER,
 
-  static const int KEY_RED;
-  static const int KEY_GREEN;
-  static const int KEY_YELLOW;
-  static const int KEY_BLUE;
+    KEY_RED,
+    KEY_GREEN,
+    KEY_YELLOW,
+    KEY_BLUE,
 
-  static const int KEY_SPACE;
-  static const int KEY_BACKSPACE;
-  static const int KEY_BACK;
-  static const int KEY_ESCAPE;
-  static const int KEY_EXIT;
+    KEY_SPACE,
+    KEY_BACKSPACE,
+    KEY_BACK,
+    KEY_ESCAPE,
+    KEY_EXIT,
 
-  static const int KEY_POWER;
-  static const int KEY_REWIND;
-  static const int KEY_STOP;
-  static const int KEY_EJECT;
-  static const int KEY_PLAY;
-  static const int KEY_RECORD;
-  static const int KEY_PAUSE;
+    KEY_POWER,
+    KEY_REWIND,
+    KEY_STOP,
+    KEY_EJECT,
+    KEY_PLAY,
+    KEY_RECORD,
+    KEY_PAUSE,
 
-  static const int KEY_GREATER_THAN_SIGN;
-  static const int KEY_LESS_THAN_SIGN;
+    KEY_GREATER_THAN_SIGN,
+    KEY_LESS_THAN_SIGN,
 
-  static const int KEY_TAB;
-  static const int KEY_TAP;
+    KEY_TAB,
+    KEY_TAP,
+  };
 
 private:
-  map<string, int> keyMap;
+  static map<string, int> keyMap;
   map<int, string> valueMap;
-
   static CodeMap *_instance;
+
   CodeMap ();
 
 public:
