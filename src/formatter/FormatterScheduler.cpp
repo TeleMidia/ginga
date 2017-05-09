5ec4f1144b79d30e808113174e304592f655594b
Antonio Busson <busson@outlook.com>
2017-05-09 12:52:14 -0300

    src/formatter: Natural end restauration

--- src/formatter/FormatterScheduler.cpp
+++ src/formatter/FormatterScheduler.cpp
@@ -1965,6 +1965,7 @@ FormatterScheduler::eventStateChanged (void *someEvent, short transition,
 
               if (hideObj)
                 {
+                  //dragon head
                   clog << "FormatterScheduler::eventStateChanged '";
                   clog << event->getId ();
                   clog << "' STOPS: hideObject '" << object->getId ();
@@ -1978,11 +1979,8 @@ FormatterScheduler::eventStateChanged (void *someEvent, short transition,
                                playerManager->getObjectPlayer (object);
                   if (player != NULL && player->getPlayer () != NULL)
                     {
-                      //dragon head
-                     // player->flip ();
-                     player->naturalEnd();
-
                       multiDevPres->stopListenPlayer (player->getPlayer ());
+                     
                     }
                 }
             }
