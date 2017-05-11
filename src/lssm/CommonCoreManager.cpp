e7710de32b076fcd7abaa4b4a394d0e7a062a509
Guilherme F. Lima <gflima@sdf.org>
2017-05-11 18:00:17 -0300

    maint: Move isdbt stuff to contrib

--- src/lssm/CommonCoreManager.cpp
+++ src/lssm/CommonCoreManager.cpp
@@ -18,23 +18,6 @@ along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 #include "ginga.h"
 #include "CommonCoreManager.h"
 
-#include "StcWrapper.h"
-
-#if WITH_ISDBT
-#include "DataWrapperListener.h"
-#include "isdbt-dataproc/DataProcessor.h"
-#include "isdbt-dataproc/DataProcessor.h"
-#include "isdbt-dataproc/IDsmccObjectListener.h"
-#include "isdbt-tsparser/Demuxer.h"
-#include "isdbt-tsparser/IDemuxer.h"
-#include "isdbt-tsparser/ITSFilter.h"
-#include "isdbt-tsparser/PipeFilter.h"
-#include "isdbt-tuner/Tuner.h"
-using namespace ::ginga::dataproc;
-using namespace ::ginga::tsparser;
-using namespace ::ginga::tuner;
-#endif
-
 #include "mb/Display.h"
 #include "mb/SDLWindow.h"
 using namespace ::ginga::mb;
@@ -64,50 +47,12 @@ CommonCoreManager::CommonCoreManager ()
 
 CommonCoreManager::~CommonCoreManager ()
 {
-  // TODO: release attributes
-  clog << "CommonCoreManager::~CommonCoreManager all done" << endl;
 }
 
 void
 CommonCoreManager::addPEM (PresentationEngineManager *pem)
 {
   this->pem = pem;
-
-#if WITH_ISDBT
-  tuner = new Tuner ();
-
-  pem->setIsLocalNcl (false, tuner);
-
-  clog << "CommonCoreManager::CommonCoreManager ";
-  clog << "creating demuxer" << endl;
-  demuxer = new Demuxer ((Tuner *)tuner);
-
-  clog << "CommonCoreManager::CommonCoreManager ";
-  clog << "creating data processor" << endl;
-
-  dataProcessor = new DataProcessor ();
-
-  ccUser = pem->getDsmccListener ();
-
-  // Add PEM as a listener of SEs and OCs
-  ((DataProcessor *)dataProcessor)
-      ->addSEListener (
-          "gingaEditingCommands",
-          (IDsmccStreamEventListener *)((DataWrapperListener *)ccUser));
-
-  ((DataProcessor *)dataProcessor)
-      ->addObjectListener (
-          (IDsmccObjectListener *)((DataWrapperListener *)ccUser));
-
-  ((DataProcessor *)dataProcessor)
-      ->setServiceDomainListener (
-          (IDsmccServiceDomainListener *)((DataWrapperListener *)ccUser));
-
-  ((DataProcessor *)dataProcessor)->setDemuxer ((IDemuxer *)demuxer);
-
-  ((Tuner *)tuner)->setLoopListener ((DataProcessor *)dataProcessor);
-
-#endif // WITH_ISDBT
 }
 
 void
@@ -131,29 +76,11 @@ CommonCoreManager::setInteractivityInfo (bool hasOCSupport)
 void
 CommonCoreManager::removeOCFilterAfterMount (arg_unused (bool removeIt))
 {
-#if WITH_ISDBT
-  if (dataProcessor != NULL)
-    {
-      ((DataProcessor *)dataProcessor)->removeOCFilterAfterMount (removeIt);
-    }
-#endif
 }
 
 void
 CommonCoreManager::setTunerSpec (arg_unused (const string &tunerSpec))
 {
-#if WITH_ISDBT
-  string ni, ch;
-  size_t pos;
-
-  pos = tunerSpec.find_first_of (":");
-  if (pos != std::string::npos)
-    {
-      ni = tunerSpec.substr (0, pos);
-      ch = tunerSpec.substr (pos + 1, tunerSpec.length () - pos + 1);
-      ((Tuner *)tuner)->setSpec (ni, ch);
-    }
-#endif
 }
 
 void
@@ -184,111 +111,12 @@ CommonCoreManager::releaseTunningWindow ()
 void
 CommonCoreManager::tune ()
 {
-#if WITH_ISDBT
-  clog << "lssm-ccm::cpi tunning..." << endl;
-  ((Tuner *)tuner)->tune ();
-#endif
 }
 
 void
 CommonCoreManager::startPresentation ()
 {
-#if !defined WITH_ISDBT || WITH_ISDBT == 0
   return;
-#else
-  IPlayer *ipav = NULL;
-  NclPlayerData *data = NULL;
-  StcWrapper *sw = NULL;
-  NetworkInterface *ni = NULL;
-  string dstUri = "dtv_channel.ts";
-
-  data = pem->createNclPlayerData ();
-
-  showTunningWindow (data->x, data->y, data->w, data->h);
-  tune ();
-  dstUri = ((IDemuxer *)demuxer)->createTSUri (dstUri);
-
-  // Create Main AV
-  ipav = createMainAVPlayer (dstUri, data->x, data->y,
-                             data->w, data->h);
-
-  delete data;
-  clog << "lssm-ccm::sp create av ok" << endl;
-
-  if (dataProcessor != NULL)
-    {
-      ni = ((Tuner *)tuner)->getCurrentInterface ();
-      if (ni != NULL && (ni->getCaps () & DPC_CAN_DECODESTC))
-        {
-          clog << "lssm-ccm::sp using stc hardware!" << endl;
-          ((DataProcessor *)dataProcessor)->setSTCProvider (ni);
-        }
-      else
-        {
-          clog << "lssm-ccm::sp using stc wrapper!" << endl;
-          sw = new StcWrapper (ipav);
-          ((DataProcessor *)dataProcessor)->setSTCProvider (sw);
-        }
-
-      nptProvider = ((DataProcessor *)dataProcessor)->getNPTProvider ();
-      if (nptProvider != NULL)
-        {
-          pem->setTimeBaseProvider ((ITimeBaseProvider *)nptProvider);
-        }
-      else
-        {
-          clog << "lssm-ccm::sp warning! can't use npt provider" << endl;
-        }
-
-      ((DataProcessor *)dataProcessor)->setNptPrinter (nptPrinter);
-      if (nptPrinter)
-        {
-          if (((IDemuxer *)demuxer)
-                  ->hasStreamType (STREAM_TYPE_DSMCC_TYPE_C))
-            {
-              ((IDemuxer *)demuxer)->setNptPrinter (nptPrinter);
-              cout << "TS HAS AN NPT STREAM" << endl;
-            }
-          else
-            {
-              cout << "NPTPRINTER WARNING!" << endl;
-              cout << "TS DOESNT HAVE A STREAM WITH NPT STREAM TYPE"
-                   << endl;
-            }
-          ((IDemuxer *)demuxer)->printPat ();
-        }
-
-      // DSM-CC descriptors
-      ((DataProcessor *)dataProcessor)
-          ->createStreamTypeSectionFilter (STREAM_TYPE_DSMCC_TYPE_D);
-
-      if (hasOCSupport)
-        {
-          ((DataProcessor *)dataProcessor)
-              ->createStreamTypeSectionFilter (STREAM_TYPE_DSMCC_TYPE_B);
-
-          ((DataProcessor *)dataProcessor)
-              ->createStreamTypeSectionFilter (STREAM_TYPE_DSMCC_TYPE_C);
-
-          // AIT
-          ((DataProcessor *)dataProcessor)
-              ->createStreamTypeSectionFilter (STREAM_TYPE_PRIVATE_SECTION);
-
-          clog << "lssm ccm::sp OC support enabled" << endl;
-        }
-      else if (nptPrinter)
-        {
-          ((DataProcessor *)dataProcessor)
-              ->createStreamTypeSectionFilter (STREAM_TYPE_DSMCC_TYPE_C);
-        }
-    }
-
-  releaseTunningWindow ();
-
-  ((IDemuxer *)demuxer)->processDemuxData ();
-
-  clog << "lssm ccm::sp all done!" << endl;
-#endif
 }
 
 GINGA_LSSM_END
