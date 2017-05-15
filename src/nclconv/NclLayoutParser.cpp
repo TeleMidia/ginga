a76fd9537ee73d6eb7840a6603fac44c57db73b8
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2017-05-15 23:07:31 +0200

    maint:  Remove NclLayoutConverter class
    * build-cmake/CMakeLists.txt: Likewise.
    * src/Makefile.am: Likewise.
    * src/nclconv/NclDocumentConverter.cpp: Likewise.
    * src/nclconv/NclLayoutParser.h: Likewise.
    * src/nclconv/NclLayoutConverter.cpp: Deleted.
    * src/nclconv/NclLayoutConverter.h: Deleted.

--- src/nclconv/NclLayoutParser.cpp
+++ src/nclconv/NclLayoutParser.cpp
@@ -18,13 +18,17 @@ along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 #include "ginga.h"
 #include "NclLayoutParser.h"
 
+#include "NclDocumentConverter.h"
+
 GINGA_PRAGMA_DIAG_IGNORE (-Wsign-conversion)
 
 GINGA_NCLCONV_BEGIN
 
-NclLayoutParser::NclLayoutParser (NclDocumentParser *documentParser)
+NclLayoutParser::NclLayoutParser (NclDocumentParser *documentParser,
+                                  DeviceLayout *deviceLayout)
     : ModuleParser (documentParser)
 {
+  this->deviceLayout = deviceLayout;
 }
 
 void *
@@ -126,6 +130,250 @@ void
 NclLayoutParser::setImportParser (NclImportParser *importParser)
 {
   this->importParser = importParser;
+}
+
+
+void
+NclLayoutParser::addImportBaseToRegionBase (void *parentObject,
+                                            void *childObject)
+{
+  map<int, RegionBase *> *bases;
+  map<int, RegionBase *>::iterator i;
+  string baseAlias, baseLocation;
+  NclDocumentConverter *compiler;
+  NclDocument *importedDocument;
+
+  // get the external base alias and location
+  baseAlias = XMLString::transcode (
+      ((DOMElement *)childObject)
+          ->getAttribute (XMLString::transcode ("alias")));
+
+  baseLocation = XMLString::transcode (
+      ((DOMElement *)childObject)
+          ->getAttribute (XMLString::transcode ("documentURI")));
+
+  compiler = (NclDocumentConverter *)getDocumentParser ();
+  importedDocument = compiler->importDocument (baseLocation);
+  if (importedDocument == NULL)
+    {
+      return;
+    }
+
+  bases = importedDocument->getRegionBases ();
+  if (bases == NULL || bases->empty ())
+    {
+      return;
+    }
+
+  // insert the imported base into the document region base
+  i = bases->begin ();
+  while (i != bases->end ())
+    {
+      ((RegionBase *)parentObject)
+          ->addBase (i->second, baseAlias, baseLocation);
+      ++i;
+    }
+}
+
+void
+NclLayoutParser::addRegionToRegion (void *parentObject,
+                                    void *childObject)
+{
+  ((LayoutRegion *)parentObject)->addRegion ((LayoutRegion *)childObject);
+}
+
+void
+NclLayoutParser::addRegionToRegionBase (void *parentObject,
+                                        void *childObject)
+{
+  RegionBase *layout = (RegionBase *)parentObject;
+  layout->addRegion ((LayoutRegion *)childObject);
+}
+
+void *
+NclLayoutParser::createRegionBase (DOMElement *parentElement,
+                                   arg_unused (void *objGrandParent))
+{
+  RegionBase *layout;
+  string mapId = "";
+
+  layout
+      = new RegionBase (XMLString::transcode (parentElement->getAttribute (
+                            XMLString::transcode ("id"))),
+
+                        deviceLayout);
+
+  // device attribute
+  if (parentElement->hasAttribute (XMLString::transcode ("device")))
+    {
+      // region for output bit map attribute
+      if (parentElement->hasAttribute (XMLString::transcode ("region")))
+        {
+          mapId = XMLString::transcode (parentElement->getAttribute (
+              XMLString::transcode ("region")));
+        }
+
+      layout->setDevice (XMLString::transcode (parentElement->getAttribute (
+                             XMLString::transcode ("device"))),
+                         mapId);
+    }
+  else
+    {
+      layout->setDevice (deviceLayout->getLayoutName (), "");
+    }
+
+  return layout;
+}
+
+void *
+NclLayoutParser::createRegion (DOMElement *parentElement,
+                               arg_unused (void *objGrandParent))
+{
+  LayoutRegion *ncmRegion;
+  string attribute;
+  double percentValue;
+
+  attribute = XMLString::transcode (
+      parentElement->getAttribute (XMLString::transcode ("id")));
+
+  // cria nova regiao
+  ncmRegion = new LayoutRegion (attribute);
+
+  // atributo title
+  if (parentElement->hasAttribute (XMLString::transcode ("title")))
+    {
+      ncmRegion->setTitle (XMLString::transcode (
+          parentElement->getAttribute (XMLString::transcode ("title"))));
+    }
+
+  // atributo: left
+  if (parentElement->hasAttribute (XMLString::transcode ("left")))
+    {
+      attribute = XMLString::transcode (
+          parentElement->getAttribute (XMLString::transcode ("left")));
+
+      if (attribute != "")
+        {
+          if (xstrispercent (attribute))
+            {
+              percentValue = getPercentValue (attribute);
+              ncmRegion->setLeft (percentValue, true);
+            }
+          else
+            {
+              double pixelValue;
+              pixelValue = getPixelValue (attribute);
+              ncmRegion->setLeft (pixelValue, false);
+            }
+        }
+    }
+
+  // atributo: right
+  if (parentElement->hasAttribute (XMLString::transcode ("right")))
+    {
+      attribute = XMLString::transcode (
+          parentElement->getAttribute (XMLString::transcode ("right")));
+
+      if (xstrispercent (attribute))
+        {
+          percentValue = getPercentValue (attribute);
+          ncmRegion->setRight (percentValue, true);
+        }
+      else
+        {
+          ncmRegion->setRight (getPixelValue (attribute), false);
+        }
+    }
+
+  // atributo: top
+  if (parentElement->hasAttribute (XMLString::transcode ("top")))
+    {
+      attribute = XMLString::transcode (
+          parentElement->getAttribute (XMLString::transcode ("top")));
+
+      if (xstrispercent (attribute))
+        {
+          percentValue = getPercentValue (attribute);
+          ncmRegion->setTop (percentValue, true);
+        }
+      else
+        {
+          ncmRegion->setTop (getPixelValue (attribute), false);
+        }
+    }
+
+  // atributo: bottom
+  if (parentElement->hasAttribute (XMLString::transcode ("bottom")))
+    {
+      attribute = XMLString::transcode (
+          parentElement->getAttribute (XMLString::transcode ("bottom")));
+
+      if (xstrispercent (attribute))
+        {
+          percentValue = getPercentValue (attribute);
+          ncmRegion->setBottom (percentValue, true);
+        }
+      else
+        {
+          ncmRegion->setBottom (getPixelValue (attribute), false);
+        }
+    }
+
+  // atributo: width
+  if (parentElement->hasAttribute (XMLString::transcode ("width")))
+    {
+      attribute = XMLString::transcode (
+          parentElement->getAttribute (XMLString::transcode ("width")));
+
+      if (xstrispercent (attribute))
+        {
+          percentValue = getPercentValue (attribute);
+          ncmRegion->setWidth (percentValue, true);
+        }
+      else
+        {
+          ncmRegion->setWidth (getPixelValue (attribute), false);
+        }
+    }
+
+  // atributo: height
+  if (parentElement->hasAttribute (XMLString::transcode ("height")))
+    {
+      attribute = XMLString::transcode (
+          parentElement->getAttribute (XMLString::transcode ("height")));
+
+      if (xstrispercent (attribute))
+        {
+          percentValue = getPercentValue (attribute);
+          ncmRegion->setHeight (percentValue, true);
+        }
+      else
+        {
+          ncmRegion->setHeight (getPixelValue (attribute), false);
+        }
+    }
+
+  if (parentElement->hasAttribute (XMLString::transcode ("zIndex")))
+    {
+      attribute = XMLString::transcode (
+          parentElement->getAttribute (XMLString::transcode ("zIndex")));
+
+      ncmRegion->setZIndex (xstrto_int (attribute));
+    }
+
+  return ncmRegion;
+}
+
+double
+NclLayoutParser::getPercentValue (const string &value)
+{
+  return xstrtodorpercent (value) * 100.;
+}
+
+int
+NclLayoutParser::getPixelValue (const string &value)
+{
+  return xstrto_int (value);
 }
 
 GINGA_NCLCONV_END
