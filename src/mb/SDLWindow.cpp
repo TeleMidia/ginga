a6e0cf2dbbc8db7a1b418bcd42a5ef4703b7b5aa
Guilherme F. Lima <gflima@sdf.org>
2017-05-29 15:07:43 -0300

    src/mb: Cleanup in SDLWindow

--- src/mb/SDLWindow.cpp
+++ src/mb/SDLWindow.cpp
@@ -20,31 +20,28 @@ along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "Display.h"
 
-#include "system/Thread.h"
-using namespace ::ginga::system;
-
-
-
 GINGA_MB_BEGIN
-
-// BEGIN SANITY ------------------------------------------------------------
 
 
 // Public methods.
 
-/**
- * Creates a window with the given position, dimensions, and z-index.
- */
-SDLWindow::SDLWindow (int x, int y, int z, int width, int height)
+SDLWindow::SDLWindow (int x, int y, int z, int w, int h)
 {
   this->mutexInit ();
   this->texture = NULL;
-  initialize (0, x, y, width, height, z);
+  this->borderWidth = 0;
+  this->bgColor = {0, 0, 0, 0};
+  this->borderColor = {0, 0, 0, 0};
+  this->rect.x = x;
+  this->rect.y = y;
+  this->rect.w = w;
+  this->rect.h = h;
+  this->z = z;
+  this->ghost = false;
+  this->visible = false;
+  this->transparencyValue = 0;
 }
 
-/**
- * Gets window texture.
- */
 SDL_Texture *
 SDLWindow::getTexture ()
 {
@@ -53,17 +50,14 @@ SDLWindow::getTexture ()
   this->lock ();
   texture = this->texture;
   this->unlock ();
+
   return texture;
 }
 
-/**
- * Sets window texture.
- */
 void
 SDLWindow::setTexture (SDL_Texture *texture)
 {
   this->lock ();
-  g_debug ("adding texture %p to window %p", texture, this);
   this->texture = texture;
   this->unlock ();
 }
@@ -71,58 +65,9 @@ SDLWindow::setTexture (SDL_Texture *texture)
 
 SDLWindow::~SDLWindow ()
 {
- // vector<SDLSurface *>::iterator i;
-
-  _lock ();
-
-  Thread::mutexDestroy (&cMutex);
-  Thread::condDestroy (&cond);
-
-  Thread::mutexDestroy (&rMutex);
-
-  _unlock ();
-  Thread::mutexDestroy (&_mutex);
-
-  clog << "SDLWindow::~SDLWindow(" << this << ") all done" << endl;
   this->mutexClear ();
 }
 
-void
-SDLWindow::initialize (arg_unused (SDLWindow* parentWindowID),
-                       int x, int y, int w, int h,
-                       int z)
-{
-  this->texture = NULL;
-
-  this->borderWidth = 0;
-  this->bgColor = {0, 0, 0, 0};
-  this->borderColor = {0, 0, 0, 0};
-
-  this->rect.x = x;
-  this->rect.y = y;
-  this->rect.w = w;
-  this->rect.h = h;
-  this->z = z;
-  this->ghost = false;
-  this->visible = false;
- 
-
-  this->transparencyValue = 0x00;
-  
-
-  Thread::mutexInit (&_mutex);
-  Thread::mutexInit (&texMutex);
-
-  Thread::mutexInit (&cMutex);
-  Thread::condInit (&cond, NULL);
-
-  Thread::mutexInit (&rMutex);
-}
-
-
-
-
-// SANITY BEGIN ------------------------------------------------------------
 double
 SDLWindow::getAlpha ()
 {
@@ -158,7 +103,6 @@ SDLWindow::setRect (SDL_Rect r)
 {
   this->rect = r;
 }
-// SANITY END --------------------------------------------------------------
 
 void
 SDLWindow::setBorder (SDL_Color c, int w)
@@ -173,9 +117,6 @@ SDLWindow::getBorder (SDL_Color *c, int *w)
   set_if_nonnull (c, this->borderColor);
   set_if_nonnull (w, this->borderWidth);
 }
-
-
-
 
 void
 SDLWindow::setBounds (int posX, int posY, int w, int h)
@@ -290,21 +231,5 @@ SDLWindow::isVisible ()
 {
   return this->visible;
 }
-
-
-void
-SDLWindow::_lock ()
-{
-  Thread::mutexLock (&_mutex);
-}
-
-void
-SDLWindow::_unlock ()
-{
-  Thread::mutexUnlock (&_mutex);
-}
-
-
-
 
 GINGA_MB_END
--- src/mb/SDLWindow.h
+++ src/mb/SDLWindow.h
@@ -18,17 +18,24 @@ along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 #ifndef SDLWINDOW_H_
 #define SDLWINDOW_H_
 
-
-#include "SDL.h"
-
 GINGA_MB_BEGIN
 
 class SDLWindow
 {
-// BEGIN SANITY ------------------------------------------------------------
 private:
   GINGA_MUTEX_DEFN ();
   SDL_Texture *texture;         // window texture
+
+  int borderWidth;
+  SDL_Color bgColor;
+  SDL_Color borderColor;
+  SDL_Rect rect;
+  gint z;
+  guint8 transparencyValue;
+  bool visible;
+  bool ghost;
+
+  void initialize (int x, int y, int w, int h, int z);
 
 public:
   SDLWindow (int, int, int, int, int);
@@ -37,46 +44,16 @@ public:
   void setTexture (SDL_Texture *);
   SDL_Texture *getTexture ();
 
-private:
- 
-  int borderWidth;
-  SDL_Color bgColor;
-  SDL_Color borderColor;
-
-  SDL_Rect rect;
-
-  gint z;
-  guint8 transparencyValue;
-  bool visible;
-  bool ghost;
-
-  pthread_mutex_t _mutex;    // external mutex
-  pthread_mutex_t texMutex; // texture mutex
-  pthread_mutex_t surMutex; // underlying surface mutex
-
-  pthread_mutex_t rMutex; // render mutex
-  pthread_mutex_t cMutex; // condition mutex
-  pthread_cond_t cond;
-
-private:
-  void initialize (SDLWindow* parentWindowID,
-                   int x, int y, int width, int height, int z);
-
-
-public:
-// SANITY BEGIN ------------------------------------------------------------
   double getAlpha ();
   void setAlpha (double);
   void setBgColor (SDL_Color);
   SDL_Color getBgColor ();
   SDL_Rect getRect ();
   void setRect (SDL_Rect);
-// SANITY END --------------------------------------------------------------
 
   void setBorder (SDL_Color, int);
   void getBorder (SDL_Color *, int *);
 
-  
   void setBounds (int x, int y, int width, int height);
   void moveTo (int x, int y);
   void resize (int width, int height);
@@ -100,13 +77,6 @@ public:
   bool isGhostWindow ();
   void setGhostWindow (bool ghost);
   bool isVisible ();
-
-
-public:
-  void _lock ();
-  void _unlock ();
-
-
 };
 
 GINGA_MB_END
