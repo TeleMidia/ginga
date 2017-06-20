5c2b2019f4e17a1f510e77fd39af64c94b9d781b
Guilherme F. Lima <gflima@sdf.org>
2017-06-20 17:13:52 -0300

    src/Player: Fix property initialization; make tests stricter

--- src/formatter/PlayerAdapter.h
+++ src/formatter/PlayerAdapter.h
@@ -75,7 +75,6 @@ private:
   void prepareScope (GingaTime offset = 0);
 
   GingaTime prepareProperties (ExecutionObject *obj);
-  void prepare (void);
   void updateObjectExpectedDuration ();
 
   bool checkRepeat (NclPresentationEvent *);
