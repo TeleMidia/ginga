6c202d8934e953f27b06af876ef22997cd46bae0
Guilherme F. Lima <gflima@sdf.org>
2017-06-21 22:13:14 -0300

    tests-ncl/samples: Disable anti-alias when drawing fps.lua grid

--- tests-ncl/samples/fps.lua
+++ tests-ncl/samples/fps.lua
@@ -5,12 +5,14 @@ _ENV = nil
 local FIRST = event.uptime ()
 local LAST = FIRST
 canvas:attrFont ('tiresias', 40, 'bold')
+canvas:attrLineWidth (1.0)
 local function redraw (e)
    local w, h = canvas:attrSize ()
    canvas:attrClip (0, 0, w, h)
    canvas:attrColor (0, 0, 0, 0)
    canvas:clear (0, 0, w, h)
    canvas:attrColor ('black')
+   canvas:attrAntiAlias ('none')
    local n = 10
    for i=0,w/n do
       canvas:drawLine (i * n, 0, i * n, h)
@@ -22,6 +24,7 @@ local function redraw (e)
    LAST = e.time
    local text = ('%.2fs\n%.2f fps'):format ((LAST-FIRST)/1000,1000/diff)
    local tw, th = canvas:measureText (text)
+   canvas:attrAntiAlias ('default')
    canvas:attrColor ('yellow')
    canvas:drawText ((w-tw)/2, (h-th)/2, text)
    canvas:attrColor ('navy')
