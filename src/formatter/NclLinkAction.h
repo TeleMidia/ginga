2f1298019e4dd93b4b46eebaf64f9cbb4be0a3dd
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2017-06-22 16:36:33 +0200

    src/formatter: Merge NclLink*Action classes in NclLinkAction.h/cpp

--- src/formatter/NclLinkAction.h
+++ src/formatter/NclLinkAction.h
@@ -19,16 +19,27 @@ along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 #define _LINKACTION_H_
 
 #include "NclEvents.h"
+#include "ncl/Action.h"
+#include "ncl/SimpleAction.h"
 
 GINGA_FORMATTER_BEGIN
 
 class NclLinkCondition;
+class NclLinkSimpleAction;
 
 class NclLinkActionProgressListener
 {
 public:
   virtual ~NclLinkActionProgressListener (){}
   virtual void actionProcessed (bool start) = 0;
+};
+
+class INclLinkActionListener
+{
+public:
+  virtual void scheduleAction (NclLinkSimpleAction *action) = 0;
+  virtual void addAction (NclLinkSimpleAction *action) = 0;
+  virtual void removeAction (NclLinkSimpleAction *action) = 0;
 };
 
 class NclLinkAction
@@ -67,6 +78,98 @@ private:
   vector<NclLinkActionProgressListener *> progressListeners;
 
   void initLinkAction (GingaTime delay);
+};
+
+class NclLinkSimpleAction : public NclLinkAction
+{
+public:
+  NclLinkSimpleAction (NclEvent *event, SimpleActionType type);
+  virtual ~NclLinkSimpleAction ();
+
+  virtual void run ();
+
+  NclEvent *getEvent ();
+  SimpleActionType getType ();
+  string getTypeString ();
+  void setSimpleActionListener (INclLinkActionListener *listener);
+  virtual vector<NclEvent *> *getEvents ();
+  virtual vector<NclLinkAction *> getImplicitRefRoleActions ();
+
+protected:
+  NclEvent *event;
+  SimpleActionType actionType;
+
+private:
+  INclLinkActionListener *listener;
+};
+
+class NclLinkRepeatAction : public NclLinkSimpleAction
+{
+public:
+  NclLinkRepeatAction (NclEvent *, SimpleActionType);
+  virtual ~NclLinkRepeatAction ();
+
+  virtual void run () override;
+
+  int getRepetitions ();
+  GingaTime getRepetitionInterval ();
+  void setRepetitions (int repetitions);
+  void setRepetitionInterval (GingaTime delay);
+
+private:
+  int repetitions;
+  GingaTime repetitionInterval;
+};
+
+class NclLinkAssignmentAction : public NclLinkRepeatAction
+{
+private:
+  string value;
+  Animation *animation;
+
+public:
+  NclLinkAssignmentAction (NclEvent *event,
+                           SimpleActionType actionType,
+                           string value);
+
+  virtual ~NclLinkAssignmentAction ();
+
+  string getValue ();
+  void setValue (const string &value);
+  Animation *getAnimation ();
+  void setAnimation (Animation *animation);
+};
+
+class NclLinkCompoundAction : public NclLinkAction,
+                              public NclLinkActionProgressListener
+{
+public:
+  NclLinkCompoundAction (short _op);
+  virtual ~NclLinkCompoundAction ();
+
+  virtual void run () override;
+
+  short getOperator ();
+  void addAction (NclLinkAction *action);
+
+  vector<NclLinkAction *> *getActions ();
+  void getSimpleActions (vector<NclLinkSimpleAction *> *simpleActions);
+  void setCompoundActionListener (INclLinkActionListener *_listener);
+
+  virtual vector<NclEvent *> *getEvents () override;
+  void actionProcessed (bool start) override;
+  virtual vector<NclLinkAction *> getImplicitRefRoleActions () override;
+
+protected:
+  vector<NclLinkAction *> _actions;
+  short _op;
+
+private:
+  int _pendingActions;
+  bool _hasStart;
+  bool _running;
+
+  INclLinkActionListener *_listener;
 };
 
 GINGA_FORMATTER_END
