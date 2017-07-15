cefe9b6fdb8729aef8cfab163d9366497329c98a
Antonio Busson <busson@outlook.com>
2017-07-15 17:03:11 -0300

    ginga-gtk: control bar

--- src-gtk/ginga-gtk.cpp
+++ src-gtk/ginga-gtk.cpp
@@ -27,15 +27,60 @@ along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 #include <gdk/gdkquartz.h>
 #endif
 
-SDL_Window *window;
-SDL_Renderer *renderer;
+//annotation
+//light #303030
+//dark  #EBEBEB
+
+typedef struct{
+  GtkWidget *toplevel_window;
+  GtkWidget *remote_control_window;
+  guint16 toplevel_container_margin = 5;   
+  GtkWidget *canvas;
+  guint16 controller_container_height = 48;
+  GtkWidget *play_button;
+  GtkWidget *stop_button;
+  GtkWidget *config_button;
+  GtkWidget *fullscreen_button;
+  GtkWidget *volume_button;
+
+}Ginga_GUI; 
+
+Ginga_GUI ginga_gui;
+
+//SDL_Window *window;
+//SDL_Renderer *renderer;
 
 static gboolean
-draw_callback (GtkWidget *widget, arg_unused (gpointer data))
-{
+resize_callback(GtkWidget *widget, gpointer data){
+  guint16 window_width = (guint16)gtk_widget_get_allocated_width(widget);
+  guint16 window_height = (guint16)gtk_widget_get_allocated_height(widget);
+  
+  gtk_widget_set_size_request(ginga_gui.canvas, 
+     window_width - ginga_gui.toplevel_container_margin,
+     window_height - ginga_gui.toplevel_container_margin - ginga_gui.controller_container_height);
+
+
+  printf("hahaha %d - %d \n", window_width, window_height);  
+  return TRUE;
+}
+
+static gboolean
+draw_callback(GtkWidget *widget, cairo_t *cr, arg_unused (gpointer data)){
+  int w, h;
+
+  w = gtk_widget_get_allocated_width (widget);
+  h = gtk_widget_get_allocated_height (widget);
+
+  cairo_set_operator (cr, CAIRO_OPERATOR_SOURCE);
+  cairo_set_source_rgb (cr, 255, 0, 0);
+  cairo_rectangle (cr, 0, 0, w, h);
+  cairo_fill (cr);
+
+ /*
   SDL_SetRenderDrawColor (renderer, 255, 0, 255, 255);
   SDL_RenderClear (renderer); 
   SDL_RenderPresent(renderer); 
+ */
 
   return TRUE;
 }
@@ -55,43 +100,90 @@ create_sdl_window_from_gtk_widget(GtkWidget *gtk_widget){
 #endif      
 }
 
+void 
+create_gtk_layout(){
+
+    // Create application window.
+    ginga_gui.toplevel_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+    g_assert_nonnull(ginga_gui.toplevel_window);
+    gtk_window_set_title (GTK_WINDOW (ginga_gui.toplevel_window), PACKAGE_STRING);
+    gtk_window_set_default_size (GTK_WINDOW (ginga_gui.toplevel_window), 800, 660);
+    gtk_window_set_position(GTK_WINDOW(ginga_gui.toplevel_window), GTK_WIN_POS_CENTER);
+    gtk_container_set_border_width(GTK_CONTAINER(ginga_gui.toplevel_window), ginga_gui.toplevel_container_margin); 
+
+    GtkWidget *fixed_layout = gtk_fixed_new( );
+    g_assert_nonnull(fixed_layout);
+    //Create Drawing area
+    ginga_gui.canvas = gtk_drawing_area_new();
+    g_assert_nonnull(ginga_gui.canvas);
+    gtk_widget_set_app_paintable(ginga_gui.canvas, TRUE);
+    g_signal_connect( ginga_gui.canvas, "draw", G_CALLBACK (draw_callback), NULL );
+    gtk_fixed_put(GTK_FIXED (fixed_layout),  ginga_gui.canvas, 0, 0);
+   
+
+ 
+
+    GtkWidget *play_icon = gtk_image_new_from_file ("/home/busson/ginga/src-gtk/icons/light-theme/play-icon.png");
+    g_assert_nonnull(play_icon);
+    ginga_gui.play_button = gtk_button_new();
+    g_assert_nonnull(ginga_gui.play_button);
+    gtk_button_set_image(GTK_BUTTON(ginga_gui.play_button), play_icon);
+    gtk_fixed_put(GTK_FIXED (fixed_layout),  ginga_gui.play_button, 0, 630);
+
+    GtkWidget *stop_icon = gtk_image_new_from_file ("/home/busson/ginga/src-gtk/icons/light-theme/stop-icon.png");
+    g_assert_nonnull(stop_icon);
+    ginga_gui.stop_button = gtk_button_new();
+    g_assert_nonnull(ginga_gui.stop_button);
+    gtk_button_set_image(GTK_BUTTON(ginga_gui.stop_button), stop_icon);
+    gtk_fixed_put(GTK_FIXED (fixed_layout),  ginga_gui.stop_button, 40, 630);
+
+    GtkWidget *time_label = gtk_label_new ("00:00");
+    gtk_label_set_markup (GTK_LABEL(time_label), g_markup_printf_escaped("<span font=\"13\"><b>\%s</b></span>", "00:00"));
+    g_assert_nonnull(time_label);
+    gtk_fixed_put(GTK_FIXED (fixed_layout),  time_label, 85, 638);
+
+   
+    GtkWidget *fullscreen_icon = gtk_image_new_from_file ("/home/busson/ginga/src-gtk/icons/light-theme/fullscreen-icon.png");
+    g_assert_nonnull(fullscreen_icon);
+    ginga_gui.fullscreen_button = gtk_button_new();
+    g_assert_nonnull(ginga_gui.fullscreen_button);
+    gtk_button_set_image(GTK_BUTTON(ginga_gui.fullscreen_button), fullscreen_icon);
+    gtk_fixed_put(GTK_FIXED (fixed_layout),  ginga_gui.fullscreen_button, 750, 630);
+
+    GtkWidget *config_icon = gtk_image_new_from_file ("/home/busson/ginga/src-gtk/icons/light-theme/settings-icon.png");
+    g_assert_nonnull(config_icon);
+    ginga_gui.config_button = gtk_button_new();
+    g_assert_nonnull(ginga_gui.config_button);
+    gtk_button_set_image(GTK_BUTTON(ginga_gui.config_button), config_icon);
+    gtk_fixed_put(GTK_FIXED (fixed_layout),  ginga_gui.config_button, 790, 630);
+    
+    ginga_gui.volume_button = gtk_volume_button_new ();
+    g_assert_nonnull(ginga_gui.volume_button);
+    gtk_fixed_put(GTK_FIXED (fixed_layout), ginga_gui.volume_button, 600, 630);
+
+    gtk_container_add(GTK_CONTAINER(ginga_gui.toplevel_window), fixed_layout);
+     // Setup GTK+ callbacks.
+    g_signal_connect(ginga_gui.toplevel_window, "check-resize", G_CALLBACK(resize_callback), NULL); 
+    g_signal_connect(ginga_gui.toplevel_window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
+  
+    gtk_widget_show_all (ginga_gui.toplevel_window);
+}
+
 int
 main (int argc, char **argv)
 {
-  GtkWidget *toplevel_window;
-
-  gtk_init (&argc, &argv);
-
-  // Create application window.
-  toplevel_window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-  g_assert_nonnull(toplevel_window);
-  gtk_window_set_title (GTK_WINDOW (toplevel_window), PACKAGE_STRING);
-  gtk_window_set_default_size (GTK_WINDOW (toplevel_window), 800, 600);
-  gtk_window_set_position(GTK_WINDOW(toplevel_window), GTK_WIN_POS_CENTER);
-  gtk_container_set_border_width(GTK_CONTAINER(toplevel_window), 5);
-  
-  //Create Drawing area
-  GtkWidget *draw_area = gtk_gl_area_new();
-//  gtk_widget_set_app_paintable(draw_area, TRUE);
-  gtk_gl_area_set_auto_render((GtkGLArea*)draw_area,TRUE);
-  g_signal_connect (draw_area, "render", G_CALLBACK (draw_callback), NULL );
-  gtk_container_add(GTK_CONTAINER(toplevel_window), draw_area);
-
-  // Setup GTK+ callbacks.
-  g_signal_connect (toplevel_window, "destroy", G_CALLBACK (gtk_main_quit), NULL);
-  
-
-  gtk_widget_show_all (toplevel_window);
+    gtk_init (&argc, &argv);
+    create_gtk_layout();
   
   // SDL TEST
-  if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+ /* if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         SDL_LogError(SDL_LOG_CATEGORY_APPLICATION, "Couldn't initialize SDL: %s", SDL_GetError());
         return 1;
   }
   window = create_sdl_window_from_gtk_widget(toplevel_window);
   g_assert_nonnull(window);
   renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE | SDL_RENDERER_TARGETTEXTURE);
-  g_assert_nonnull(renderer);  
+  g_assert_nonnull(renderer);  */
 
   // Enter event loop.
   gtk_main ();
