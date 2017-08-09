e516ce9f099c5d725df578f8f0bf1e10ed17fa6e
Guilherme F. Lima <gflima@sdf.org>
2017-08-09 13:04:41 -0300

    maint: Fix warnings

--- src/ginga.cpp
+++ src/ginga.cpp
@@ -16,6 +16,8 @@ You should have received a copy of the GNU General Public License
 along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include <config.h>
+#include <string.h>
+
 #include <cairo.h>
 #include <glib.h>
 #include <glib/gstdio.h>
