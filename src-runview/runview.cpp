dbd44715e83b2cae88dbf570f769c6056554a7ba
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2017-09-25 22:15:26 +0200

    src/runview:  Some clean up

--- src-runview/runview.cpp
+++ src-runview/runview.cpp
@@ -1,86 +1,72 @@
 #include "runview.h"
 #include "ui_runview.h"
 
-#include <cairo.h>
 #include <glib.h>
-#include <stdlib.h>
-#include <glib/gstdio.h>
 
-#include "ginga.h"
-
-#include <QDebug>
-#include <QTimer>
 #include <QPainter>
 #include <QPaintEvent>
 
 using namespace std;
 
-// Global formatter.
-static Ginga *GINGA = nullptr;
-GingaOptions opts;
-
-cairo_surface_t *surface;
-cairo_t *cr;
-
 RunView::RunView (QWidget *parent) :
   QWidget (parent),
-  _ui (new Ui::RunView)
+  _ui (new Ui::RunView),
+  _timer (this)
 {
   _ui->setupUi(this);
 
-  setUpdatesEnabled (true);
+  _ginga_surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 800, 600);
+  _cr = cairo_create (_ginga_surface);
 
-  surface = cairo_image_surface_create (CAIRO_FORMAT_ARGB32, 800, 600);
-  cr = cairo_create (surface);
-
-  opts.debug = FALSE;
-  opts.width = 800;
-  opts.height = 600;
-  opts.experimental = FALSE;
-  opts.background = "black";
+  _ginga_opts.debug = FALSE;
+  _ginga_opts.width = 800;
+  _ginga_opts.height = 600;
+  _ginga_opts.experimental = FALSE;
+  _ginga_opts.background = "black";
 
   char **argv;
-  GINGA = Ginga::create (0, argv, &opts);
+  _ginga = Ginga::create (0, argv, &_ginga_opts);
 
-  g_assert_nonnull (GINGA);
+  g_assert_nonnull (_ginga);
+
+  connect (&_timer, SIGNAL(timeout()), this, SLOT(redrawGinga()));
 }
 
 void
 RunView::start (const string &file)
 {
   string errmsg;
-  if (!GINGA->start (file, &errmsg))
+  if (!_ginga->start (file, &errmsg))
     {
       g_printerr ("error: ");
       g_printerr ("%s\n", errmsg.c_str ());
     }
 
-  QTimer *timer = new QTimer(this);
-  connect (timer, SIGNAL(timeout()), this, SLOT(redrawGinga()));
-  timer->start (33);
+  _timer.start (33);
 }
 
 void
 RunView::stop ()
 {
-  GINGA->stop ();
-  close ();
+  _ginga->stop ();
+  _timer.stop();
 }
 
 RunView::~RunView()
 {
   delete _ui;
+  delete _ginga;
 }
 
 void
 RunView::redrawGinga ()
 {
-  GINGA->redraw (cr);
-  img = QImage (cairo_image_surface_get_data (surface),
-                cairo_image_surface_get_width (surface),
-                cairo_image_surface_get_height (surface),
+  _ginga->redraw (_cr);
+  _img = QImage (cairo_image_surface_get_data (_ginga_surface),
+                cairo_image_surface_get_width (_ginga_surface),
+                cairo_image_surface_get_height (_ginga_surface),
                 QImage::Format_ARGB32_Premultiplied);
-  g_assert_cmpuint(img.bytesPerLine(), ==, cairo_image_surface_get_stride(surface));
+  g_assert_cmpuint(_img.bytesPerLine(), ==, cairo_image_surface_get_stride(_ginga_surface));
 
   update ();
 }
@@ -90,8 +76,8 @@ RunView::paintEvent (QPaintEvent *e)
 {
   QPainter painter (this);
 
-  if (!img.isNull())
+  if (!_img.isNull())
     {
-      painter.drawImage (0, 0, img);
+      painter.drawImage (0, 0, _img);
     }
 }
--- src-runview/runview.h
+++ src-runview/runview.h
@@ -3,6 +3,9 @@
 
 #include <string>
 #include <QWidget>
+#include <QTimer>
+
+#include "ginga.h"
 
 namespace Ui {
   class RunView;
@@ -13,8 +16,8 @@ class RunView : public QWidget
   Q_OBJECT
 
 public:
-  explicit RunView(QWidget *parent = 0);
-  virtual ~RunView();
+  explicit RunView (QWidget *parent = 0);
+  virtual ~RunView ();
 
   void start (const std::string &);
   void stop ();
@@ -26,8 +29,15 @@ protected Q_SLOTS:
   void redrawGinga ();
 
 private:
+  Ginga *_ginga;
+  GingaOptions _ginga_opts;
+
+  cairo_surface_t *_ginga_surface;
+  cairo_t *_cr;
+
   Ui::RunView *_ui;
-  QImage img;
+  QImage _img;
+  QTimer _timer;
 };
 
 #endif // WIDGET_H
