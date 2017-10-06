a2d8964b60f3f6559127d915e821c8d4eb676cb2
Roberto Azevedo <robertogerson@telemidia.puc-rio.br>
2017-10-06 17:08:24 +0200

    lib/gl: Change default texture format to GL_BGRA_EXT

--- lib/aux-ginga.h
+++ lib/aux-ginga.h
@@ -230,7 +230,8 @@ xvectremove (vector<T> &v1, vector<T> &v2)
 # include <GLES2/gl2.h>
 # include <GLES2/gl2ext.h>
 #else
-#include <GL/gl.h>
+# include <GL/gl.h>
+# include <GL/glext.h>
 #endif
 
 #define CHECK_GL_ERROR()                                        \
