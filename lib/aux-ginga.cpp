cec2d240855d0a4f4f4147a47cd06b02c6a6850b
Guilherme F. Lima <gflima@sdf.org>
2017-11-10 11:06:01 -0200

    lib: Remove unused includes from aux-ginga.h

--- lib/aux-ginga.cpp
+++ lib/aux-ginga.cpp
@@ -16,6 +16,7 @@ You should have received a copy of the GNU General Public License
 along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
 
 #include "aux-ginga.h"
+#include <sstream>              // for stringstream
 
 GINGA_NAMESPACE_BEGIN
 
