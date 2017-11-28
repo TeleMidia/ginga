858aeaa9818d77d3be26a944664e876c30557e7c
Guilherme F. Lima <gflima@sdf.org>
2017-11-28 21:35:14 -0200

    tests: Add test-Document-*

---
+++ tests/test-Document-new.cpp
@@ -0,0 +1,47 @@
+/* Copyright (C) 2006-2017 PUC-Rio/Laboratorio TeleMidia
+
+This file is part of Ginga (Ginga-NCL).
+
+Ginga is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Ginga is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include "Document.h"
+#include "Context.h"
+#include "Media.h"
+#include "MediaSettings.h"
+#include "Switch.h"
+
+int
+main (void)
+{
+  Document *doc;
+
+  doc = new Document ();
+  g_assert_nonnull (doc);
+  g_assert (doc->getObjects ()->size () == 2);
+  g_assert (doc->getMedias ()->size () == 1);
+  g_assert (doc->getContexts ()->size () == 1);
+  g_assert (doc->getSwitches ()->size () == 0);
+
+  Context *root = doc->getRoot ();
+  g_assert_nonnull (root);
+  g_assert (root->getId () == "__root__");
+
+  MediaSettings *settings = doc->getSettings ();
+  g_assert_nonnull (settings);
+  g_assert (settings->getId () == "__settings__");
+
+  delete doc;
+
+  exit (EXIT_SUCCESS);
+}
