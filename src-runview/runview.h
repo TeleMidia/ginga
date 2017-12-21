852b89713fc5b4b7819006aebc72fe8163880202
Guilherme F. Lima <gflima@sdf.org>
2017-12-21 13:25:33 -0200

    src-gui,src-runview: Fix some headers

--- src-runview/runview.h
+++ src-runview/runview.h
@@ -1,5 +1,22 @@
-#ifndef WIDGET_H
-#define WIDGET_H
+/* Copyright (C) 2006-2017 PUC-Rio/Laboratorio TeleMidia
+
+This file is part of Ginga (Ginga-NCL).
+
+Ginga is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Ginga is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Ginga.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#ifndef RUNVIEW_H
+#define RUNVIEW_H
 
 #include <string>
 #include <QWidget>
@@ -43,4 +60,4 @@ private:
   QElapsedTimer _elapsedTime;
 };
 
-#endif // WIDGET_H
+#endif // RUNVIEW_H
