465bfeeb1f7cec5a944f484556cf1e28c6eb86b8
jvgirard <jv.gsnunes@gmail.com>
2018-01-17 15:53:12 -0200

    maint: PlayerSigGen now uses appsink instead of autovideosink

--- lib/player/PlayerSigGen.h
+++ lib/player/PlayerSigGen.h
@@ -31,36 +31,39 @@ public:
   void stop () override;
   void pause () override;
   void resume () override;
+  void redraw (cairo_t *) override;
 
 protected:
   bool doSetProperty (PlayerProperty, const string &,
                       const string &) override;
 
 private:
-  GstElement *_pipeline;        // pipeline
-  struct {                      // audio pipeline
-    GstElement *src;            // Audio Test Src format
-    GstElement *convert;        // convert audio format
-    GstElement *tee;            // splits pipeline
-    GstElement *audioQueue;     // links audio pipeline side 
-    GstElement *audioSink;      // audio sink
-    GstElement *videoQueue;     // links video pipeline side 
-    GstElement *videoScope;     // video draw style
-    GstElement *videoConvert;   // convert video format
-    GstElement *videoSink;      // video sink
+  GstElement *_pipeline;          // pipeline
+  struct {                        // audio pipeline
+    GstElement *src;              // Audio Test Src format
+    GstElement *convert;          // convert audio format
+    GstElement *tee;              // splits pipeline
+    GstElement *audioQueue;       // links audio pipeline side 
+    GstElement *audioSink;        // audio sink
+    GstElement *videoQueue;       // links video pipeline side 
+    GstElement *videoScope;       // video draw style
+    GstElement *videoConvert;     // convert video format
+    GstElement *videoSink;        // video sink
 
-    GstPad *teeAudioPad;        // tee audio pad (output)
-    GstPad *queueAudioPad;      // queue audio pad (input)
-    GstPad *teeVideoPad;        // tee video pad (output)
-    GstPad *queueVideoPad;      // queue video pad (input)
+    GstPad *teeAudioPad;          // tee audio pad (output)
+    GstPad *queueAudioPad;        // queue audio pad (input)
+    GstPad *teeVideoPad;          // tee video pad (output)
+    GstPad *queueVideoPad;        // queue video pad (input)
   } _audio;
-  int _sample_flag;             // true if new sample is available
+  
+  int _sample_flag;               // true if new sample is available
+  GstAppSinkCallbacks _callbacks; // video app-sink callback data
 
   struct
   {
-    double freq;                // frequency
-    int wave;                   // wave
-    double volume;              // sound level
+    double freq;                  // frequency
+    int wave;                     // wave
+    double volume;                // sound level
   } _prop;
 
   // GStreamer callbacks.
