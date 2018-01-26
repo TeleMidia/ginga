dedb387bb466c96717a2d6baee83d4304c52b266
Alan Livio <alan@telemidia.puc-rio.br>
2018-01-25 22:51:23 -0300

    tests-ncl: Add unit tests for naturalend of Context.

---
+++ tests/test-Context-naturalend.cpp
@@ -0,0 +1,124 @@
+/* Copyright (Ctx) 2006-2017 PUC-Rio/Laboratorio TeleMidia
+
+This file is part of Ginga (Ginga-NCL).
+
+Ginga is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Ginga is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Ginga.  If not, see <https://www.gnu.org/licenses/>.  */
+
+#include "Event.h"
+#include "Media.h"
+#include "Context.h"
+#include "Parser.h"
+
+
+#define PARSE_AND_START(fmt, doc, str)                                     \
+  G_STMT_START                                                             \
+  {                                                                        \
+    string buf = str;                                                      \
+    string errmsg;                                                         \
+    *fmt = new Formatter (0, nullptr, nullptr);                            \
+    g_assert_nonnull (*fmt);                                               \
+    if (!(*fmt)->start (buf.c_str (), buf.length (), &errmsg))             \
+      {                                                                    \
+        g_printerr ("*** Unexpected error: %s", errmsg.c_str ());          \
+        g_assert_not_reached ();                                           \
+      }                                                                    \
+    *doc = (*fmt)->getDocument ();                                         \
+    g_assert_nonnull (*doc);                                               \
+  }                                                                        \
+  G_STMT_END
+
+int
+main (void)
+{
+
+  // Presentation events ---------------------------------------------------
+
+  // @lambda: START from state OCCURRING.
+  {
+    Formatter *fmt;
+    Document *doc;
+    PARSE_AND_START (&fmt, &doc, "\
+<ncl>\n\
+  <head>\n\
+  </head>\n\
+  <body>\n\
+    <port id='start1' component='ctx1'/>\n\
+    <context id='ctx1'>\n\
+      <port id='port1' component='m1'/>\n\
+      <media id='m1'>\n\
+        <area id='a1' begin='3s'/>\n\
+      </media>\n\
+    </context>\n\
+  </body>\n\
+</ncl>\n");
+
+    Event *root_lambda = doc->getRoot ()->getLambda ();
+    g_assert_nonnull (root_lambda);
+    Context *ctx1 = cast (Context *, doc->getObjectById ("ctx1"));
+    g_assert_nonnull (ctx1);
+    Event *ctx1_lambda = ctx1->getLambda ();
+    g_assert_nonnull (ctx1_lambda);
+    Event *m1_lambda = *ctx1->getPorts ()->begin ();
+    g_assert_nonnull (m1_lambda);
+
+    // --------------------------------
+    // check start document
+
+    // when start the document, only the lambda@root is OCCURING
+    g_assert (root_lambda->getState () == Event::OCCURRING);
+    g_assert (ctx1_lambda->getState () == Event::SLEEPING);
+    g_assert (m1_lambda->getState () == Event::SLEEPING);
+
+    fmt->sendTick (0, 0, 0);
+    g_assert (root_lambda->getState () == Event::OCCURRING);
+    g_assert (ctx1_lambda->getState () == Event::OCCURRING);
+    g_assert (m1_lambda->getState () == Event::SLEEPING);
+
+    // in next reaction, lambda@ctx1 and its anchors
+    // are OCCURRING, and its properties are SLEEPING
+    fmt->sendTick (0, 0, 0);
+    g_assert (root_lambda->getState () == Event::OCCURRING);
+    g_assert (ctx1_lambda->getState () == Event::OCCURRING);
+    g_assert (m1_lambda->getState () == Event::OCCURRING);
+
+    // --------------------------------
+    // main check
+
+    // after stop m1, m1 is SLEEPING
+    g_assert_true (m1_lambda->transition (Event::STOP));
+    g_assert (root_lambda->getState () == Event::OCCURRING);
+    g_assert (ctx1_lambda->getState () == Event::OCCURRING);
+    g_assert (m1_lambda->getState () == Event::SLEEPING);
+
+    // in next reaction, ctx1 is SLEEPING
+    fmt->sendTick (0, 0, 0);
+    g_assert (root_lambda->getState () == Event::OCCURRING);
+    g_assert (ctx1_lambda->getState () == Event::SLEEPING);
+    g_assert (m1_lambda->getState () == Event::SLEEPING);
+
+    // in next reaction, root is SLEEPING
+    fmt->sendTick (0, 0, 0);
+    g_assert (root_lambda->getState () == Event::SLEEPING);
+    g_assert (ctx1_lambda->getState () == Event::SLEEPING);
+    g_assert (m1_lambda->getState () == Event::SLEEPING);
+
+    // in next reaction, fmt is stoped
+    fmt->sendTick (0, 0, 0);
+    g_assert (fmt->getState () == GINGA_STATE_STOPPED);
+
+    delete fmt;
+  }
+
+  exit (EXIT_SUCCESS);
+}
