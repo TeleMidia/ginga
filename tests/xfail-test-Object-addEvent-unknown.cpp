8a9140bcec8a781c8faed81e18122bb2281a2f73
Alan Livio <alan@telemidia.puc-rio.br>
2018-02-03 22:45:19 -0200

    tests: Add xfail-test-Object-addEvent-unknown.

---
+++ tests/xfail-test-Object-addEvent-unknown.cpp
@@ -0,0 +1,49 @@
+/* Copyright (C) 2006-2018 PUC-Rio/Laboratorio TeleMidia
+
+This file is part of Ginga (Ginga-NCL).
+
+Ginga is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+Ginga is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Ginga.  If not, see <https://www.gnu.org/licenses/>.  */
+
+#include "Object.h"
+#include "Context.h"
+#include "Media.h"
+#include "MediaSettings.h"
+#include "Switch.h"
+
+class MockMedia : public Media
+{
+public:
+  MockMedia (const string &id) : Media (id)
+  {
+  }
+  set<Event *> *
+  getEvents ()
+  {
+    return &_events;
+  }
+};
+
+int
+main (void)
+{
+  // Add no standart event
+  MockMedia *mock = new MockMedia ("mock");
+  set<Event *> *evts = mock->getEvents ();
+  Event *evt
+      = new Event (static_cast<Event::Type> (10), mock, "unknown_evt");
+  evts->insert (evt);
+  mock->toString ();
+
+  exit (EXIT_SUCCESS);
+}
