a142e848be72f53bd87d480b5f32e02fc9711e54
Jorge Dodsworth <jorgedods@gmail.com>
2018-02-05 14:48:03 -0200

    feat: add parse_media and parse_port functions

--- lib/ParserLua.cpp
+++ lib/ParserLua.cpp
@@ -33,6 +33,162 @@ GINGA_NAMESPACE_BEGIN
 
 // Parsing functions.
 
+// having a pointer problem (line 61)
+// parse_port (doc, parent, port)
+static int
+l_parse_port (lua_State* L)
+{
+  Object *obj;
+  Event *event;
+  Document *doc;
+  Context *parent;
+  string str;
+  string id;
+  string evt;
+  size_t at;
+
+  doc = (Document *) lua_touserdata (L, 1);
+  parent = (Context *) lua_touserdata (L, 2);
+  g_assert_nonnull(parent);
+  str = string(luaL_checkstring (L, -1));
+
+  at = str.find('@');
+
+  if (at != str.npos) // presentation
+    {
+      id = str.substr(0, at);
+      evt = str.substr(at, str.npos);
+      obj = parent->getChildById (id);
+printf (">> %s - %s\n", id.c_str(), evt.c_str());
+TRACE("%s", obj->toString().c_str()); // only lambda should have '@' as prefix?
+      event = obj->getEvent (Event::PRESENTATION, evt);
+    }
+  else                          // attribution
+    {
+      // items = xstrsplit (str, '.');
+      // id = items.front();
+      // evt = items.back();
+      // obj = parent->getChildById (id);
+      // event = obj->getEvent (Event::ATTRIBUTION, evt);
+    }
+
+  g_assert_nonnull(event);
+  parent->addPort (event);
+
+  return 0;
+}
+
+// label not implemented for area list
+// parse_media (doc, parent, t)
+static int
+l_parse_media (lua_State* L)
+{
+  Media *media;
+  Document *doc;
+  Composition *parent;
+  const char *tag;
+  const char *id;
+  const char *mime;
+  const char *uri;
+  const char *name;
+  const char *value;
+  Time begin, end;
+
+  doc = (Document *) lua_touserdata (L, 1);
+  parent = (Composition *) lua_touserdata (L, 2);
+
+  luaL_checktype (L, 3, LUA_TTABLE);
+  lua_rawgeti (L, 3, 1);
+  tag = luaL_checkstring (L, -1);
+
+  if (!g_str_equal (tag, "media"))
+    {
+      lua_pushfstring (L, "unexpected tag: %s", tag);
+      lua_error (L);
+    }
+
+  lua_rawgeti (L, 3, 2);
+  id = luaL_checkstring (L, -1);
+
+  lua_rawgeti (L, 3, 3);
+  if (lua_isnil (L, -1) == 0)   // have property list
+    {
+      lua_getfield (L, 6, "mime");
+      mime = luaL_optstring (L, -1, "");
+
+      lua_getfield (L, 6, "src");
+      uri = luaL_optstring (L, -1, "");
+
+      media = new Media (id, mime, uri);
+
+      lua_pushnil (L);
+      while (lua_next(L, 6) != 0)
+        {
+          name = lua_tolstring (L, -2, 0);
+          value = lua_tolstring (L, -1, 0);
+          if ((name != "mime") && (name != "src")) {
+            media->addAttributionEvent (name);
+            media->setProperty (name, value);
+          }
+
+          lua_pop(L, 1);
+        }
+      lua_pop(L, 2);
+    }
+  else
+    {
+      media = new Media (id, "", "");
+    }
+
+  lua_pop(L, 1);
+  lua_rawgeti (L, 3, 4);
+  if (lua_isnil (L, -1) == 0)   // have area list
+    {
+      lua_pushnil (L);
+      while (lua_next(L, 6) != 0)
+        {
+          name = lua_tolstring (L, -2, 0);
+
+          lua_rawgeti (L, -1, 1);
+          value = lua_tolstring (L, -1, 0);
+
+          begin = 0;
+          if ((value != NULL) &&
+              (unlikely (!ginga::try_parse_time (value, &begin))))
+            {
+              lua_pushfstring (L, "bad attr: %s", value);
+              lua_error (L);
+            }
+
+          lua_pop(L, 1);
+          lua_rawgeti (L, -1, 2);
+          value = lua_tolstring (L, -1, 0);
+
+          end = GINGA_TIME_NONE;
+          if ((value != NULL) &&
+              (unlikely (!ginga::try_parse_time (value, &end))))
+            {
+              lua_pushfstring (L, "bad attr: %s", value);
+              lua_error (L);
+            }
+          media->addPresentationEvent (name, begin, end);
+          lua_pop(L, 2);
+        }
+    }
+
+  if (parent == NULL)        // error
+    {
+      lua_pushfstring (L, "parent missing: %s", id);
+      lua_error (L);
+    }
+  else                          // add parent
+    {
+      parent->addChild(media);
+    }
+
+  return 0;
+}
+
 // parse_context (doc, [parent], t)
 static int
 l_parse_context (lua_State *L)
@@ -43,6 +199,7 @@ l_parse_context (lua_State *L)
   const char *id;
 
   doc = (Document *) lua_touserdata (L, 1);
+  g_assert_nonnull(doc);
   parent = (Composition *) lua_touserdata (L, 2);
 
   luaL_checktype (L, 3, LUA_TTABLE);
@@ -58,14 +215,45 @@ l_parse_context (lua_State *L)
   lua_rawgeti (L, 3, 2);
   id = luaL_checkstring (L, -1);
 
-  if (parent == nullptr)        // root
+  if (parent == NULL)        // root
     {
       Context *root = doc->getRoot ();
       root->addAlias (string (id));
+      parent = root;
     }
   else                          // non-root
     {
       g_assert_not_reached ();
+    }
+
+  lua_rawgeti (L, 3, 4);
+  if (lua_isnil (L, -1) == 0)   // media table
+    {
+      lua_pushnil (L);
+      while (lua_next(L, -2) != 0)
+        {
+          lua_pushcfunction (L, l_parse_media);
+          lua_pushlightuserdata (L, doc);
+          lua_pushlightuserdata (L, parent);
+          lua_pushvalue (L, -4);
+          lua_call (L, 3, 0);
+          lua_pop (L, 1);
+        }
+    }
+
+  lua_rawgeti (L, 3, 3);
+  if (lua_isnil (L, -1) == 0)   // port table
+    {
+      lua_pushnil (L);
+      while (lua_next(L, -2) != 0)
+        {
+          lua_pushcfunction (L, l_parse_port);
+          lua_pushlightuserdata (L, doc);
+          lua_pushlightuserdata (L, parent);
+          lua_pushvalue (L, -4);
+          lua_call (L, 3, 0);
+          lua_pop (L, 1);
+        }
     }
 
   return 0;
@@ -83,13 +271,13 @@ process (lua_State *L, string *errmsg)
   doc = new Document ();
   lua_pushcfunction (L, l_parse_context);
   lua_pushlightuserdata (L, doc);
-  lua_pushlightuserdata (L, nullptr);
+  lua_pushlightuserdata (L, NULL);
   lua_pushvalue (L, 1);
   if (unlikely (lua_pcall (L, 3, 0, 0) != LUA_OK))
     {
       delete doc;
       tryset (errmsg, g_strdup (luaL_checkstring (L, -1)));
-      return nullptr;
+      return NULL;
     }
 
   return doc;
@@ -118,7 +306,7 @@ ParserLua::parseBuffer (const void *buf, size_t size, string *errmsg)
   str = g_strndup ((const gchar *) buf, size);
   g_assert_nonnull (str);
 
-  doc = nullptr;
+  doc = NULL;
   err = luaL_dostring (L, str);
   if (unlikely (err != LUA_OK))
     {
@@ -151,7 +339,7 @@ ParserLua::parseFile (const string &path, string *errmsg)
   g_assert_nonnull (L);
   luaL_openlibs (L);
 
-  doc = nullptr;
+  doc = NULL;
   err = luaL_dofile (L, path.c_str ());
   if (unlikely (err != LUA_OK))
     {
