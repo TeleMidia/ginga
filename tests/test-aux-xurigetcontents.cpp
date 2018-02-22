2d1d50c4505a97932a8cb31a7459d601196267ba
Alan Livio <alan@telemidia.puc-rio.br>
2018-02-22 14:16:44 -0300

    tests: Refactory test-aux-xurigetcontents.cpp

--- tests/test-aux-xurigetcontents.cpp
+++ tests/test-aux-xurigetcontents.cpp
@@ -21,7 +21,9 @@ along with Ginga.  If not, see <https://www.gnu.org/licenses/>.  */
   "PUC-Rio/TeleMidia Lab.    www.telemidia.puc-rio.br\n"
 #define AUTHORS_FILE_REMOTE_URI                                            \
   "http://raw.githubusercontent.com/TeleMidia/ginga/master/AUTHORS"
-#define AUTHORS_FILE_LOCAL_RELATIVE_PATH TOP_SRCDIR "/AUTHORS"
+#define AUTHORS_FILE_LOCAL_RELATIVE_PATH TOP_SRCDIR G_DIR_SEPARATOR_S "AUTHORS"
+#define AUTHORS_FILE_LOCAL_ABS_PATH                                        \
+  ABS_TOP_SRCDIR G_DIR_SEPARATOR_S "AUTHORS"
 
 int
 main (void)
@@ -32,27 +34,27 @@ main (void)
   {
     // It must return false and not change the contents string.
     string empty = "empty";
-    g_assert (xurigetcontents ("unknown", empty) == false);
-    g_assert (empty
-              == "empty"); // doesn't change the string passed as param.
+    g_assert_false (xurigetcontents ("unknown", empty));
+    // doesn't change the string passed as param.
+    g_assert (empty == "empty");
   }
 
   // relative path. (also invalid uri.)
   {
     // It must return false and not change the contents string.
     string empty = "empty";
-    g_assert (xurigetcontents (AUTHORS_FILE_LOCAL_RELATIVE_PATH, empty)
-              == false);
-    g_assert (empty
-              == "empty"); // doesn't change the string passed as param.
+    g_assert_false (
+        xurigetcontents (AUTHORS_FILE_LOCAL_RELATIVE_PATH, empty));
+    // doesn't change the string passed as param.
+    g_assert (empty == "empty");
   }
 
   // valid fullpath. (also invalid uri.)
   {
     // It must return false and not change the contents string.
     string empty = "empty";
-    string abs = xpathmakeabs (AUTHORS_FILE_LOCAL_RELATIVE_PATH);
-    g_assert (xurigetcontents (abs, empty) == false);
+    bool ret = xurigetcontents (AUTHORS_FILE_LOCAL_ABS_PATH, empty);
+    g_assert_false (ret);
     g_assert (empty == "empty");
   }
 
@@ -63,8 +65,8 @@ main (void)
     // It must return true and work just fine.
     GError *err = nullptr;
     string content = "";
-    string abs = xpathmakeabs (AUTHORS_FILE_LOCAL_RELATIVE_PATH);
-    gchar *uri_local = g_filename_to_uri (abs.c_str (), NULL, &err);
+    gchar *uri_local
+        = g_filename_to_uri (AUTHORS_FILE_LOCAL_ABS_PATH, NULL, &err);
     g_assert_null (err);
 
     g_assert (xurigetcontents (uri_local, content));
