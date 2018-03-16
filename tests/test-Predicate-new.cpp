83dab06cf4ad9fdbc3a1724ed5def274b102c3df
Alan Livio <alan@telemidia.puc-rio.br>
2018-03-16 15:51:12 -0300

    tests: Refactory tests/test-Predicate-new

--- tests/test-Predicate-new.cpp
+++ tests/test-Predicate-new.cpp
@@ -17,20 +17,126 @@ along with Ginga.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #include "tests.h"
 
+#define CHECK_TEST(pred, exp_left, exp_test, exp_right)                    \
+  G_STMT_START                                                             \
+  {                                                                        \
+    Predicate::Test test;                                                  \
+    string left;                                                           \
+    string right;                                                          \
+    (pred)->getTest (&left, &test, &right);                                \
+    g_assert (left == (exp_left));                                         \
+    g_assert (test == (exp_test));                                         \
+    g_assert (right == (exp_right));                                       \
+  }                                                                        \
+  G_STMT_END
+
 int
 main (void)
 {
-  list<Predicate::Type> types = {
-    Predicate::FALSUM,   Predicate::VERUM,       Predicate::ATOM,
-    Predicate::NEGATION, Predicate::CONJUNCTION, Predicate::DISJUNCTION,
-  };
+  // Predicate::ATOM
+  {
+    Predicate *pred;
 
-  for (auto type : types)
-    {
-      Predicate *pred = new Predicate (type);
-      g_assert (pred->getType () == type);
-      delete pred;
-    }
+    pred = new Predicate (Predicate::ATOM);
+    g_assert (pred->getType () == Predicate::ATOM);
+    g_assert (pred->getChildren ()->size () == 0);
+
+    CHECK_TEST (pred, "", Predicate::EQ, "");
+
+    pred->setTest ("a", Predicate::EQ, "b");
+    CHECK_TEST (pred, "a", Predicate::EQ, "b");
+
+    pred->setTest ("x", Predicate::NE, "y");
+    CHECK_TEST (pred, "x", Predicate::NE, "y");
+
+    pred->setTest ("a", Predicate::LT, "b");
+    CHECK_TEST (pred, "a", Predicate::LT, "b");
+
+    pred->setTest ("x", Predicate::LE, "y");
+    CHECK_TEST (pred, "x", Predicate::LE, "y");
+
+    pred->setTest ("a", Predicate::GT, "b");
+    CHECK_TEST (pred, "a", Predicate::GT, "b");
+
+    pred->setTest ("x", Predicate::GE, "y");
+    CHECK_TEST (pred, "x", Predicate::GE, "y");
+
+    delete pred;
+  }
+
+  // Predicate::FALSUM
+  {
+    Predicate *pred;
+
+    pred = new Predicate (Predicate::FALSUM);
+    g_assert (pred->getType () == Predicate::FALSUM);
+
+    delete pred;
+  }
+
+  // Predicate::VERUM
+  {
+    Predicate *pred;
+
+    pred = new Predicate (Predicate::VERUM);
+    g_assert (pred->getType () == Predicate::VERUM);
+    g_assert (pred->getChildren ()->size () == 0);
+
+    delete pred;
+  }
+
+  // Predicate::NEGATION
+  {
+    Predicate *pred;
+
+    pred = new Predicate (Predicate::NEGATION);
+    g_assert (pred->getType () == Predicate::NEGATION);
+    g_assert (pred->getChildren ()->size () == 0);
+
+    delete pred;
+  }
+
+  // Predicate::CONJUNCTION
+  {
+    Predicate *pred, *child1, *child2;
+
+    pred = new Predicate (Predicate::CONJUNCTION);
+    g_assert (pred->getType () == Predicate::CONJUNCTION);
+    g_assert (pred->getChildren ()->size () == 0);
+
+    child1 = new Predicate (Predicate::ATOM);
+    child1->setTest ("a", Predicate::EQ, "a");
+    pred->addChild (child1);
+    g_assert (pred->getChildren ()->size () == 1);
+
+    child2 = new Predicate (Predicate::ATOM);
+    child2->setTest ("b", Predicate::EQ, "b");
+    pred->addChild (child2);
+    g_assert (pred->getChildren ()->size () == 2);
+
+    delete pred;
+  }
+
+  // Predicate::DISJUNCTION
+  {
+    Predicate *pred, *child1, *child2;
+
+    pred = new Predicate (Predicate::DISJUNCTION);
+    g_assert (pred->getType () == Predicate::DISJUNCTION);
+    g_assert (pred->getChildren ()->size () == 0);
+
+    child1 = new Predicate (Predicate::ATOM);
+    child1->setTest ("a", Predicate::EQ, "a");
+    pred->addChild (child1);
+    g_assert (pred->getChildren ()->size () == 1);
+
+    child2 = new Predicate (Predicate::ATOM);
+    child2->setTest ("b", Predicate::EQ, "b");
+    pred->addChild (child2);
+    g_assert (pred->getChildren ()->size () == 2);
+
+    delete pred;
+  }
 
   exit (EXIT_SUCCESS);
 }
