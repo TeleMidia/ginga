5f74319598f2eba7da9214887e5240bca63809e3
Jorge Pumar Dodsworth <jorgepd@telemidia.puc-rio.br>
2018-03-23 11:46:14 -0300

    test: add tests for ParserLua.cpp

--- tests/test-ParserLua-parseBuffer.cpp
+++ tests/test-ParserLua-parseBuffer.cpp
@@ -1,122 +1,172 @@
 /* Copyright (C) 2006-2018 PUC-Rio/Laboratorio TeleMidia
 
-This file is part of Ginga (Ginga-NCL).
+   This file is part of Ginga (Ginga-NCL).
 
-Ginga is free software: you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 2 of the License, or
-(at your option) any later version.
+   Ginga is free software: you can redistribute it and/or modify it
+   under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 2 of the License, or
+   (at your option) any later version.
 
-Ginga is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
-License for more details.
+   Ginga is distributed in the hope that it will be useful, but WITHOUT
+   ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+   or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public
+   License for more details.
 
-You should have received a copy of the GNU General Public License
-along with Ginga.  If not, see <https://www.gnu.org/licenses/>.  */
+   You should have received a copy of the GNU General Public License
+   along with Ginga.  If not, see <https://www.gnu.org/licenses/>.  */
 
 #include "tests.h"
-
-static bool
-check_failure (const string &log, const string &expected, const string &buf)
-{
-  static int i = 1;
-  Document *doc;
-  string msg = "";
-
-  g_printerr ("XFAIL #%d: %s\n", i++, log.c_str ());
-  doc = ParserLua::parseBuffer (buf.c_str (), buf.length (), &msg);
-  if (doc != nullptr)
-    {
-      delete doc;
-      return false;
-    }
-
-  if (expected != "" && !xstrhassuffix (msg, expected))
-    {
-      g_printerr ("*** Expected:\t\"%s\"\n", expected.c_str ());
-      g_printerr ("*** Got:\t\"%s\"\n", msg.c_str ());
-      return false;
-    }
-
-  g_printerr ("\n");
-  return true;
-}
-
-static Document *
-check_success (const string &log, const string &buf)
-{
-  static int i = 1;
-  Document *doc;
-  string msg = "";
-
-  g_printerr ("PASS #%d: %s\n", i++, log.c_str ());
-  doc = ParserLua::parseBuffer (buf.c_str (), buf.length (), &msg);
-  if (msg != "")
-    {
-      g_printerr ("*** Unexpected error: %s", msg.c_str ());
-      g_assert_not_reached ();
-    }
-  return doc;
-}
-
-#define XFAIL(log, exp, str) g_assert (check_failure ((log), (exp), (str)))
-
-#define PASS(obj, log, str)                                                \
-  G_STMT_START                                                             \
-  {                                                                        \
-    tryset (obj, check_success ((log), (str)));                            \
-    g_assert_nonnull (*(obj));                                             \
-  }                                                                        \
-  G_STMT_END
 
 int
 main (void)
 {
-  string tmp;
+  Formatter *fmt;
 
   // -------------------------------------------------------------------------
-  // General errors.
+  // Sanity checks.
   // -------------------------------------------------------------------------
 
-  XFAIL ("Syntax error", "unexpected symbol near '<'", "<a>");
-
-  XFAIL ("Bad body",
-         "", // ignored
-         "return 0");
-
-  XFAIL ("Bad body",
-         "", // ignored
-         "return nil");
-
-  XFAIL ("Bad body",
-         "", // ignored
-         "return {}");
-
-  XFAIL ("Bad body", "unexpected tag: unknown", "return {'unknown', 5}");
-
-  // Success: Empty body.
+  // Success: Context
   {
+    Formatter *fmt;
     Document *doc;
-    PASS (&doc, "Empty body", "\
-return {'context', 'hello'}\
-");
+
+    tests_parse_and_start (&fmt, &doc, "ncl =\
+{'context', 'ncl', nil,\n                    \
+   {\n                                       \
+      {'context', 'c1', nil,\n               \
+       {{'media', 'm1'}}},\n                 \
+      {'media', 'm2'}\n                      \
+   }\n                                       \
+}\n                                          \
+return ncl",
+"lua");
+
     g_assert_nonnull (doc);
+    g_assert (doc->getObjects ()->size () == 5);
+    g_assert (doc->getMedias ()->size () == 3);
+    g_assert (doc->getContexts ()->size () == 2);
 
-    MediaSettings *settings = doc->getSettings ();
-    g_assert_nonnull (settings);
+    Context *body = cast (Context *, doc->getRoot ());
+    g_assert_nonnull (body);
+    Event *body_lambda = body->getLambda ();
+    g_assert_nonnull (body_lambda);
 
-    Context *root = doc->getRoot ();
-    g_assert_nonnull (root);
-    g_assert (root->getId () == "__root__");
-    g_assert (root->hasAlias ("hello"));
-    g_assert (root->getPorts ()->size () == 0);
-    g_assert (root->getChildren ()->size () == 1);
-    g_assert (root->getLinks ()->size () == 0);
+    Context *c1 = cast (Context *, body->getChildById ("c1"));
+    g_assert_nonnull (c1);
+    Event *c1_lambda = c1->getLambda ();
+    g_assert_nonnull (c1_lambda);
 
-    g_assert (doc->getMedias ()->size () == 1);
+    Media *m1 = cast (Media *, c1->getChildById ("m1"));
+    g_assert_nonnull (m1);
+    Event *m1_lambda = m1->getLambda ();
+    g_assert_nonnull (m1_lambda);
+
+    Media *m2 = cast (Media *, body->getChildById ("m2"));
+    g_assert_nonnull (m2);
+    Event *m2_lambda = m2->getLambda ();
+    g_assert_nonnull (m2_lambda);
+
     delete doc;
   }
+
+  // Success: Switch
+  {
+    Formatter *fmt;
+    Document *doc;
+
+    tests_parse_and_start (&fmt, &doc, "ncl =\
+{'context', 'ncl', nil,\n                    \
+   {\n                                       \
+      {'switch', 's1',\n                     \
+       {{'media', 'm1'}}},\n                 \
+      {'media', 'm2'}\n                      \
+   }\n                                       \
+}\n                                          \
+return ncl",
+"lua");
+
+    g_assert_nonnull (doc);
+    g_assert (doc->getObjects ()->size () == 5);
+    g_assert (doc->getMedias ()->size () == 3);
+    g_assert (doc->getContexts ()->size () == 1);
+    g_assert (doc->getSwitches ()->size () == 1);
+
+    Context *body = cast (Context *, doc->getRoot ());
+    g_assert_nonnull (body);
+    Event *body_lambda = body->getLambda ();
+    g_assert_nonnull (body_lambda);
+
+    Switch *s1 = cast (Switch *, body->getChildById ("s1"));
+    g_assert_nonnull (s1);
+    Event *s1_lambda = s1->getLambda ();
+    g_assert_nonnull (s1_lambda);
+
+    Media *m1 = cast (Media *, s1->getChildById ("m1"));
+    g_assert_nonnull (m1);
+    Event *m1_lambda = m1->getLambda ();
+    g_assert_nonnull (m1_lambda);
+
+    Media *m2 = cast (Media *, body->getChildById ("m2"));
+    g_assert_nonnull (m2);
+    Event *m2_lambda = m2->getLambda ();
+    g_assert_nonnull (m2_lambda);
+
+    delete doc;
+  }
+
+  // Success: Media
+  // missing media property test
+  {
+    Document *doc;
+
+    tests_parse_and_start (&fmt, &doc, "ncl =\
+{'context', 'ncl', nil,\n                    \
+   {\n                                       \
+      {'media', 'm1'},\n                     \
+      {'media', 'm2', nil,\n                 \
+       {a1={'5s', '8s'},\n                   \
+        a2={'1s', nil}}}\n                   \
+   }\n                                       \
+}\n                                          \
+return ncl",
+"lua");
+
+    g_assert_nonnull (doc);
+    g_assert (doc->getObjects ()->size () == 4);
+    g_assert (doc->getMedias ()->size () == 3);
+    g_assert (doc->getContexts ()->size () == 1);
+
+    Context *body = cast (Context *, doc->getRoot ());
+    g_assert_nonnull (body);
+    Event *body_lambda = body->getLambda ();
+    g_assert_nonnull (body_lambda);
+
+    Media *m1 = cast (Media *, body->getChildById ("m1"));
+    g_assert_nonnull (m1);
+    Event *m1_lambda = m1->getLambda ();
+    g_assert_nonnull (m1_lambda);
+
+    Media *m2 = cast (Media *, body->getChildById ("m2"));
+    g_assert_nonnull (m2);
+    Event *m2_lambda = m2->getLambda ();
+    g_assert_nonnull (m2_lambda);
+    Event *m2_a1 = m2->getPresentationEvent ("a1");
+    g_assert_nonnull (m2_a1);
+    Event *m2_a2 = m2->getPresentationEvent ("a2");
+    g_assert_nonnull (m2_a2);
+
+    delete doc;
+  }
+
+  // Success: Link
+  {}
+
+  // Success: Predicate
+  {}
+
+  // Success: Switch rule
+  {}
 
   exit (EXIT_SUCCESS);
 }
