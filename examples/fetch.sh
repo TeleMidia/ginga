8c2a52587766a038f09c2d70be1bb5c90c338d7c
Alan Livio <alan@telemidia.puc-rio.br>
2018-03-27 17:17:33 -0300

    examples: Update fetch.sh to download patchs from github.com/andrelbd1/ginga_aplications

--- examples/fetch.sh
+++ examples/fetch.sh
@@ -29,10 +29,18 @@ APPS="$APPS velha"
 APPS="$APPS vivamais-peso"
 
 URI=http://www.telemidia.puc-rio.br/~gflima/misc/nclapps
+URI_PATCHS=https://raw.githubusercontent.com/andrelbd1/ginga_aplications/master/
+# declare -A PATCHS
+PATCHS="comerciais-proview/main.ncl comerciais-proview/connbase.ncl formula1/formula1.ncl hackerteen/hackerteen.lua hackerteen/hackerteen.ncl matrix/matrix.ncl matrixEstresse30.conn orchestra/orchestra.ncl pixelation/pixelation.ncl proderj/proderj.ncl roteiro-do-dia/connectorBase.ncl roteiro-do-dia/descriptorBase.ncl roteiro-do-dia/main.ncl roteiro-dlso-dia/regionBase.ncl velha/connbase.ncl velha/main.ncl vivamais-alim/vivamais.ncl vivamais-alim/vivamais30.conn vivamais-peso/peso.deps vivamais-peso/peso.lua"
+
 for app in $APPS; do
-  wget --show-progress -q -r -N -np -nH --cut-dirs 3\
-       -R "index.html*" "$URI/$app/"
+  wget --show-progress -q -r -N -np -nH --cut-dirs 3 \
+    -R "index.html*" "$URI/$app/"
 done
 
 git clone https://github.com/robertogerson/nclua-tutorial.git
 
+for patch in $PATCHS; do
+  rm $patch
+  wget $URI_PATCHS/$patch -P $(dirname $patch)/
+done
