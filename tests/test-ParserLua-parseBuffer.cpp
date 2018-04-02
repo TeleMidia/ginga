f775d7f849266d8dd66f121ff66fce8a2e3bfca3
Jorge Pumar Dodsworth <jorgepd@telemidia.puc-rio.br>
2018-04-02 09:51:22 -0300

    maint: fix missing action predicate

--- tests/test-ParserLua-parseBuffer.cpp
+++ tests/test-ParserLua-parseBuffer.cpp
@@ -17,32 +17,54 @@
 
 #include "tests.h"
 
+static Document *
+check_success (const string &log, const string &buf)
+{
+  static int i = 1;
+  Document *doc;
+  string msg = "";
+
+  g_printerr ("PASS #%d: %s\n", i++, log.c_str ());
+  doc = ParserLua::parseBuffer (buf.c_str (), buf.length (), &msg);
+  if (msg != "")
+    {
+      g_printerr ("*** Unexpected error: %s", msg.c_str ());
+      g_assert_not_reached ();
+    }
+  return doc;
+}
+
+#define PASS(obj, log, str)                                                \
+  G_STMT_START                                                             \
+  {                                                                        \
+    tryset (obj, check_success ((log), (str)));                            \
+    g_assert_nonnull (*(obj));                                             \
+  }                                                                        \
+  G_STMT_END
+
 // Remember to do more thorough testing on every component
 
 int
 main (void)
 {
-  Formatter *fmt;
-
   // -------------------------------------------------------------------------
   // Sanity checks.
   // -------------------------------------------------------------------------
 
   // Success: Context
   {
-    Formatter *fmt;
     Document *doc;
 
-    tests_parse_and_start (&fmt, &doc, "ncl =\
-{'context', 'ncl', nil,\n                    \
+    PASS (&doc, "Context check", "ncl =\
+{'context', 'ncl',\n                         \
+   {'m2@lambda'},\n                          \
    {\n                                       \
       {'context', 'c1', nil,\n               \
        {{'media', 'm1'}}},\n                 \
       {'media', 'm2'}\n                      \
    }\n                                       \
 }\n                                          \
-return ncl",
-                           "lua");
+return ncl");
 
     g_assert_nonnull (doc);
     g_assert (doc->getObjects ()->size () == 5);
@@ -74,30 +96,38 @@ return ncl",
 
   // Success: Switch
   {
-    Formatter *fmt;
     Document *doc;
 
-    tests_parse_and_start (&fmt, &doc, "ncl =\
+    PASS (&doc, "Switch check", "ncl =\
 {'context', 'ncl', nil,\n                    \
    {\n                                       \
       {'switch', 's1',\n                     \
-       {{'media', 'm1'}}},\n                 \
+       {\n                                   \
+        {'media', 'm1'},\n                   \
+        {'context', 'c1'},\n                 \
+        {'switch', 's2'}\n                   \
+       }\n                                   \
+      },\n                                   \
       {'media', 'm2'}\n                      \
    }\n                                       \
 }\n                                          \
-return ncl",
-                           "lua");
+return ncl");
 
     g_assert_nonnull (doc);
-    g_assert (doc->getObjects ()->size () == 5);
+    g_assert (doc->getObjects ()->size () == 7);
     g_assert (doc->getMedias ()->size () == 3);
-    g_assert (doc->getContexts ()->size () == 1);
-    g_assert (doc->getSwitches ()->size () == 1);
+    g_assert (doc->getContexts ()->size () == 2);
+    g_assert (doc->getSwitches ()->size () == 2);
 
     Context *body = cast (Context *, doc->getRoot ());
     g_assert_nonnull (body);
     Event *body_lambda = body->getLambda ();
     g_assert_nonnull (body_lambda);
+
+    Media *m2 = cast (Media *, body->getChildById ("m2"));
+    g_assert_nonnull (m2);
+    Event *m2_lambda = m2->getLambda ();
+    g_assert_nonnull (m2_lambda);
 
     Switch *s1 = cast (Switch *, body->getChildById ("s1"));
     g_assert_nonnull (s1);
@@ -109,10 +139,15 @@ return ncl",
     Event *m1_lambda = m1->getLambda ();
     g_assert_nonnull (m1_lambda);
 
-    Media *m2 = cast (Media *, body->getChildById ("m2"));
-    g_assert_nonnull (m2);
-    Event *m2_lambda = m2->getLambda ();
-    g_assert_nonnull (m2_lambda);
+    Context *c1 = cast (Context *, s1->getChildById ("c1"));
+    g_assert_nonnull (c1);
+    Event *c1_lambda = c1->getLambda ();
+    g_assert_nonnull (c1_lambda);
+
+    Switch *s2 = cast (Switch *, s1->getChildById ("s2"));
+    g_assert_nonnull (s2);
+    Event *s2_lambda = s2->getLambda ();
+    g_assert_nonnull (s2_lambda);
 
     delete doc;
   }
@@ -121,17 +156,23 @@ return ncl",
   {
     Document *doc;
 
-    tests_parse_and_start (&fmt, &doc, "ncl =\
+    PASS (&doc, "Media check", "ncl =\
 {'context', 'ncl', nil,\n                    \
    {\n                                       \
-      {'media', 'm1'},\n                     \
+      {'media', 'm1',\n                      \
+       {src='path/to/file.txt',\n            \
+        transIn={type='barWipe',\n           \
+                 dur='1s'},\n                \
+        transOut={type='barWipe',\n          \
+                  dur='1s'}\n                \
+       }\n                                   \
+      },\n                                   \
       {'media', 'm2', nil,\n                 \
        {a1={'5s', '8s'},\n                   \
         a2={'1s', nil}}}\n                   \
    }\n                                       \
 }\n                                          \
-return ncl",
-                           "lua");
+return ncl");
 
     g_assert_nonnull (doc);
     g_assert (doc->getObjects ()->size () == 4);
@@ -164,18 +205,17 @@ return ncl",
   {
     Document *doc;
 
-    tests_parse_and_start (&fmt, &doc, "ncl = \
+    PASS (&doc, "Link check", "ncl = \
 {'context', 'ncl', nil,\n                     \
    {\n                                        \
       {'media', 'm1'},\n                      \
    },\n                                       \
-   {\n                                        \
-      {{{'start', 'm1@lambda', {true}}},\n    \
-      {{'stop', 'm1@lambda'}}}\n              \
-   }\n                                        \
+   {{\n                                       \
+      {{'start', 'm1@lambda', {true}}},\n     \
+      {{'stop', 'm1@lambda'}}\n               \
+   }}\n                                       \
 }\n                                           \
-return ncl",
-                           "lua");
+return ncl");
 
     g_assert_nonnull (doc);
     g_assert (doc->getObjects ()->size () == 3);
@@ -201,18 +241,19 @@ return ncl",
   {
     Document *doc;
 
-    tests_parse_and_start (&fmt, &doc, "ncl = \
+    PASS (&doc, "Predicate check", "ncl = \
 {'context', 'ncl', nil,\n                     \
    {\n                                        \
       {'media', 'm1'},\n                      \
    },\n                                       \
    {\n                                        \
-      {{{'start', 'm1@lambda', {true}}},\n    \
+      {{{'start', 'm1@lambda',\n              \
+         {'or', {'2', '>=', '1'}, {true}}\n   \
+       }},\n                                  \
       {{'stop', 'm1@lambda'}}}\n              \
    }\n                                        \
 }\n                                           \
-return ncl",
-                           "lua");
+return ncl");
 
     g_assert_nonnull (doc);
     g_assert (doc->getObjects ()->size () == 3);
@@ -234,17 +275,16 @@ return ncl",
     list<Action> cond = body->getLinks ()->front ().first;
     Predicate *pred = cond.front ().predicate;
 
-    g_assert (pred->getType () == Predicate::VERUM);
+    g_assert (pred->getType () == Predicate::DISJUNCTION);
 
     delete doc;
   }
 
   // Success: Switch rule
   {
-    Formatter *fmt;
     Document *doc;
 
-    tests_parse_and_start (&fmt, &doc, "ncl =\
+    PASS (&doc, "Switch rule check", "ncl =\
 {'context', 'ncl', nil,\n                    \
    {\n                                       \
       {'switch', 's1',\n                     \
@@ -254,8 +294,7 @@ return ncl",
       {'media', 'm2'}\n                      \
    }\n                                       \
 }\n                                          \
-return ncl",
-                           "lua");
+return ncl");
 
     g_assert_nonnull (doc);
     g_assert (doc->getObjects ()->size () == 5);
