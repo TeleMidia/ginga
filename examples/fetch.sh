939c3512e62b81b30afb55755c021ec45b92a518
André Brandão <andrelbd1@gmail.com>
2018-04-12 15:58:28 -0300

    examples: Adding "roteiro-do-dia" app.  * examples/fetch.sh

--- examples/fetch.sh
+++ examples/fetch.sh
@@ -23,6 +23,7 @@ APPS="$APPS hackerteen"
 APPS="$APPS orchestra"
 APPS="$APPS pixelation"
 APPS="$APPS proderj"
+APPS="$APPS roteiro-do-dia"
 APPS="$APPS velha"
 APPS="$APPS velha"
 APPS="$APPS vivamais-peso"
@@ -30,7 +31,7 @@ APPS="$APPS vivamais-alim"
 
 URI=http://www.telemidia.puc-rio.br/~gflima/misc/nclapps
 URI_PATCHS=https://raw.githubusercontent.com/andrelbd1/ginga_aplications/master/
-PATCHS="comerciais-proview/main.ncl comerciais-proview/connbase.ncl comerciais-proview/app_cover.png comerciais-proview/app_thumbnail.png formula1/formula1.ncl formula1/app_cover.png formula1/app_thumbnail.png hackerteen/hackerteen.lua hackerteen/hackerteen.ncl hackerteen/app_cover.png hackerteen/app_thumbnail.png orchestra/orchestra.ncl orchestra/app_cover.png orchestra/app_thumbnail.png pixelation/pixelation.ncl pixelation/app_cover.png pixelation/app_thumbnail.png proderj/proderj.ncl proderj/app_cover.png proderj/app_thumbnail.png roteiro-do-dia/connectorBase.ncl roteiro-do-dia/descriptorBase.ncl roteiro-do-dia/main.ncl roteiro-dlso-dia/regionBase.ncl roteiro-do-dia/app_cover.png roteiro-do-dia/app_thumbnail.png velha/connbase.ncl velha/main.ncl velha/app_cover.png velha/app_thumbnail.png vivamais-alim/vivamais.ncl vivamais-alim/vivamais30.conn vivamais-alim/app_cover.png vivamais-alim/app_thumbnail.png vivamais-peso/peso.deps vivamais-peso/peso.lua vivamais-peso/app_cover.png vivamais-peso/app_thumbnail.png pacman/app_cover.png pacman/app_thumbnail.png"
+PATCHS="comerciais-proview/main.ncl comerciais-proview/connbase.ncl comerciais-proview/app_cover.png comerciais-proview/app_thumbnail.png formula1/formula1.ncl formula1/app_cover.png formula1/app_thumbnail.png hackerteen/hackerteen.lua hackerteen/hackerteen.ncl hackerteen/app_cover.png hackerteen/app_thumbnail.png orchestra/orchestra.ncl orchestra/app_cover.png orchestra/app_thumbnail.png pixelation/pixelation.ncl pixelation/app_cover.png pixelation/app_thumbnail.png proderj/proderj.ncl proderj/app_cover.png proderj/app_thumbnail.png roteiro-do-dia/connectorBase.ncl roteiro-do-dia/descriptorBase.ncl roteiro-do-dia/main.ncl roteiro-do-dia/regionBase.ncl roteiro-do-dia/app_cover.png roteiro-do-dia/app_thumbnail.png velha/connbase.ncl velha/main.ncl velha/app_cover.png velha/app_thumbnail.png vivamais-alim/vivamais.ncl vivamais-alim/vivamais30.conn vivamais-alim/app_cover.png vivamais-alim/app_thumbnail.png vivamais-peso/peso.deps vivamais-peso/peso.lua vivamais-peso/app_cover.png vivamais-peso/app_thumbnail.png pacman/app_cover.png pacman/app_thumbnail.png"
 
 for app in $APPS; do
   wget --show-progress -q -r -N -np -nH --cut-dirs 3 \
