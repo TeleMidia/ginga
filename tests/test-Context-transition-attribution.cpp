9c7309e6a0a881994712b9261c7008b506925059
Alan Livio <alan@telemidia.puc-rio.br>
2018-04-22 17:06:00 -0300

    tests: Increase test-{Context,Media}-transition-attribution

--- tests/test-Context-transition-attribution.cpp
+++ tests/test-Context-transition-attribution.cpp
@@ -20,5 +20,50 @@ along with Ginga.  If not, see <https://www.gnu.org/licenses/>.  */
 int
 main (void)
 {
+  {
+    // ABORT prop from state OCCURRING
+    // ABORT prop from state PAUSED
+    // ABORT prop from state SLEEPING
+
+    // PAUSE prop from state OCCURRING
+    // PAUSE prop from state PAUSED
+    // PAUSE prop from state SLEEPING
+
+    // START prop from state OCCURRING
+    // START prop from state PAUSED
+    // START prop from state SLEEPING
+    {
+      Formatter *fmt;
+      Event *body_lambda, *c1_lambda, *c1_prop, *m1_lambda, *m2_lambda;
+
+      tests_create_document_with_context_and_start (
+          &fmt, &body_lambda, &c1_lambda, &c1_prop, &m1_lambda, &m2_lambda);
+
+      // START AttributionEvent is done and return true
+      g_assert_true (c1_prop->transition (Event::START));
+
+      // after START AttributionEvent is OCCURRING
+      g_assert (body_lambda->getState () == Event::OCCURRING);
+      g_assert (c1_lambda->getState () == Event::OCCURRING);
+      g_assert (m1_lambda->getState () == Event::OCCURRING);
+      g_assert (m2_lambda->getState () == Event::OCCURRING);
+      g_assert (c1_prop->getState () == Event::OCCURRING);
+
+      // when advance time AttributionEvent is SLEEPING
+      fmt->sendTick (0, 0, 0);
+      g_assert (body_lambda->getState () == Event::OCCURRING);
+      g_assert (c1_lambda->getState () == Event::OCCURRING);
+      g_assert (m1_lambda->getState () == Event::OCCURRING);
+      g_assert (m2_lambda->getState () == Event::OCCURRING);
+      g_assert (c1_prop->getState () == Event::SLEEPING);
+
+      delete fmt;
+    }
+
+    // STOP lambda from state OCCURRING
+    // STOP lambda from state PAUSED
+    // STOP lambda from state SLEEPING
+  }
+
   exit (EXIT_SUCCESS);
 }
--- tests/test-Media-transition-attribution.cpp
+++ tests/test-Media-transition-attribution.cpp
@@ -20,5 +20,48 @@ along with Ginga.  If not, see <https://www.gnu.org/licenses/>.  */
 int
 main (void)
 {
+
+  // ABORT prop from state OCCURRING
+  // ABORT prop from state PAUSED
+  // ABORT prop from state SLEEPING
+
+  // PAUSE prop from state OCCURRING
+  // PAUSE prop from state PAUSED
+  // PAUSE prop from state SLEEPING
+
+  // START prop from state OCCURRING
+  // START prop from state PAUSED
+  // START prop from state SLEEPING
+  {
+    Formatter *fmt;
+    Event *body_lambda, *m1_lambda, *m1_anchor_0s, *m1_label, *m1_prop;
+
+    tests_create_document_with_media_and_start (
+        &fmt, &body_lambda, &m1_lambda, &m1_anchor_0s, &m1_label, &m1_prop);
+
+    // START is done and return true
+    g_assert_true (m1_prop->transition (Event::START));
+
+    // after START m1_lambada and m1_anchor_0s are OCCURRING
+    g_assert (body_lambda->getState () == Event::OCCURRING);
+    g_assert (m1_lambda->getState () == Event::OCCURRING);
+    g_assert (m1_anchor_0s->getState () == Event::OCCURRING);
+    g_assert (m1_label->getState () == Event::SLEEPING);
+    g_assert (m1_prop->getState () == Event::OCCURRING);
+
+    // when advance time AttributionEvent is SLEEPING
+    fmt->sendTick (0, 0, 0);
+    g_assert (body_lambda->getState () == Event::OCCURRING);
+    g_assert (m1_lambda->getState () == Event::OCCURRING);
+    g_assert (m1_anchor_0s->getState () == Event::OCCURRING);
+    g_assert (m1_label->getState () == Event::SLEEPING);
+    g_assert (m1_prop->getState () == Event::SLEEPING);
+
+    delete fmt;
+  }
+
+  // STOP lambda from state OCCURRING
+  // STOP lambda from state PAUSED
+  // STOP lambda from state SLEEPING
   exit (EXIT_SUCCESS);
 }
