837a53e52dab138d45c95e627368b24a7e7018c4
Guilherme F. Lima <gflima@sdf.org>
2019-03-29 19:57:45 -0300

    src/behavior: Add parAnd and parOr

--- src/behavior.lua
+++ src/behavior.lua
@@ -1,9 +1,14 @@
+---
+-- The Behavior class.
+-- @classmod Behavior
+
 local assert       = assert
 local coroutine    = coroutine
 local debug        = debug
 local error        = error
 local getmetatable = getmetatable
 local ipairs       = ipairs
+local pairs        = pairs
 local print        = print
 local setmetatable = setmetatable
 local table        = table
@@ -16,21 +21,73 @@ do
    behavior.__index = behavior
 end
 
+local function args2str (...)
+   local res = {}
+   for _,v in ipairs {...} do
+      table.insert (res, tostring (v))
+   end
+   return table.concat (res, ',')
+end
+
+local function list2str (t)
+   return '{'..args2str (table.unpack (t))..'}'
+end
+
+local function tab2str (t)
+   local res = {}
+   for k,v in pairs (t) do
+      if type (k) == 'string' then
+         k = "'"..k.."'"
+      else
+         k = tostring (k)
+      end
+      if type (v) == 'string' then
+         v = "'"..v.."'"
+      else
+         v = tostring (v)
+      end
+      table.insert (res, ('[%s]=%s'):format (k,v))
+   end
+   return '{'..table.concat (res, ',')..'}'
+end
+
+-- Default error handler.
+local function bhvError (bhv, message)
+   error (tostring (bhv)..': '..message, 2)
+end
+
+-- Auxiliary function used by behavior._dump().
+local function bhvDump (prefix, node, indent, highlight)
+   local me = ('  '):rep (indent or 0)..tostring (node)
+   if node == highlight then
+      me = me..'*'
+   end
+   if node:_isRoot () then
+      me = me..list2str (node.stack)
+   end
+   print (prefix..me)
+   for _,child in ipairs (node.children) do
+      bhvDump (prefix, child, indent + 1, highlight)
+   end
+end
+
+-- Creates a new behavior.
 local function bhvNew (f, name, parent)
    local co = assert (coroutine.create (f))
    local t = {
       co       = co,                    -- the underlying coroutine
       name     = name or tostring (co), -- behavior name
       parent   = parent,                -- parent behavior
-      children = {},                    -- list of child trails
-      error    = error,                 -- error handler
-      trace    = false,                 -- whether to trace calls
+      children = {},                    -- list of child behaviors
+      awaiting = false,                 -- blocked on await
+      emitting = false,                 -- blocked on emit
+      canrun   = nil,                   -- stack level of last emission
    }
    local bhv = setmetatable (t, behavior)
    if parent == nil then        -- this is root
-      bhv.root   = bhv
+      bhv.root = bhv
       bhv.co2bhv = {[co]=bhv}   -- maps coroutine to behavior
-      bhv.stack  = {}           -- stack of events
+      bhv.stack = {}            -- stack of events
    else
       assert (getmetatable (parent) == behavior)
       bhv.root = assert (parent.root)
@@ -40,42 +97,28 @@ local function bhvNew (f, name, parent)
    return bhv
 end
 
+-- Removes the i-th child of behavior.
 local function bhvRemoveChild (bhv, i)
-   local child = assert (table.remove (bhv.root.children, i))
+   local child = assert (table.remove (bhv.children, i))
    bhv.root.co2bhv[child.co] = nil
 end
 
-local function dump (prefix, node, indent, highlight)
-   local me = ('  '):rep (indent or 0)..tostring (node)
-   if node == highlight then
-      me = me..'*'
-   end
-   if node:_isRoot () then
-      local evts = {}
-      for _,v in ipairs (node.stack) do
-         table.insert (evts, tostring (v))
-      end
-      me = me .. (' #%d: {%s}')
-         :format (#node.stack, table.concat (evts, ','))
-   end
-   print (prefix..me)
-   for _,child in ipairs (node.children) do
-      dump (prefix, child, indent + 1, highlight)
-   end
-end
-
+
 -- Private -----------------------------------------------------------------
 
+-- Prints trace message (if tracing is on).
 function behavior:_trace (fmt, ...)
-   if self.trace then
-      print (self.name..'\t'..(fmt):format (...))
+   if self.root.trace then
+      print (self.name..':'..(fmt):format (...))
    end
 end
 
+-- Dumps behavior tree to stdout.
 function behavior:_dump (prefix)
-   dump (prefix or '', self.root, 0, self)
+   bhvDump (prefix or '', self.root, 0, self)
 end
 
+-- Returns a string representing behavior.
 function behavior:__tostring ()
    local status
    if self.awaiting then
@@ -89,37 +132,70 @@ function behavior:__tostring ()
                                 coroutine.status (self.co), status)
 end
 
+-- Tests whether behavior is root.
 function behavior:_isRoot ()
    return self == self.root
 end
 
+-- Tests whether behavior is a parallel composition.
+function behavior:_isPar (kind)
+   if kind == nil then
+      return self.kind ~= nil
+   else
+      return self.kind == kind
+   end
+end
+
+-- Gets the behavior associated with coroutine co.
+-- If CO is nil, gets the behavior associated with the current coroutine.
 function behavior:_getBehavior (co)
    return self.root.co2bhv[co or coroutine.running ()]
 end
 
+-- Pushes event into event stack.
 function behavior:_push (evt)
    table.insert (self.root.stack, assert (evt))
 end
 
+-- Pops event from event stack.
 function behavior:_pop (evt)
    return assert (table.remove (self.root.stack))
 end
 
-function behavior:_resume (evt)
-   local status, result = coroutine.resume (self.co, evt)
+-- Resumes behavior with the given arguments.
+-- Throws an error in case of failure.
+function behavior:_resume (...)
+   assert (coroutine.status (self.co) == 'suspended')
+   self:_trace ('_resume (%s)', args2str (...))
+   local status, result = coroutine.resume (self.co, ...)
    if not status then
       error (result)
    end
    return result
 end
 
+-- Resumes behavior with the given arguments.
+-- Calls the error handler in case of failure.
+function behavior:_resumeSafe (...)
+   assert (coroutine.status (self.co) == 'suspended')
+   self:_trace ('_resumeSafe (%s)', args2str (...))
+   local status, result = coroutine.resume (self.co, ...)
+   if not status then
+      assert (self.root.error) (self, result)
+      return nil
+   end
+   return result
+end
+
+-- Broadcasts event to behavior tree.
 function behavior:_broadcast (evt)
    assert (evt)
    if self.emitting then
-      return                    -- skip trail
+      return                    -- skip behavior
    end
    self:_trace ('_broadcast (%s)', evt)
-   if self.awaiting then
+   if self.awaiting and not self:_isPar () then
+      self:_trace (' ...trying to awake: %s', self)
       self:_resume (evt)
    else
       for _,child in ipairs (self.children) do
@@ -128,114 +204,227 @@ function behavior:_broadcast (evt)
    end
 end
 
+
 -- Public ------------------------------------------------------------------
 
-function behavior.init (name)
-   local func = function (self)
-      while true do
-         local evt = coroutine.yield ()
-         self:_push (evt or true) -- push sentinel if there is no event
-         if evt then
-            self:_broadcast (evt)
-         end
-         self:_trace ('external cycle (%s)', evt)
-         while #self.stack > 0 do
-            self:_trace ('internal cycle (%s)', evt)
-            --self:_dump ((' '):rep (4)..'\t|')
-            local progress = false
-            local i = 1
-            while i <= #self.children do
-               local child = assert (self.children[i])
-               if not child.awaiting and not child.emitting then
-                  progress = true -- child will advance
-               end
-               self:_trace ('... resuming trail: %s', child)
-               local status, result = coroutine.resume (child.co)
-               if not status then
-                  self.error (('%s: %s'):format (child, result))
-                  result = nil  -- force removal
-               end
+local function bhvInitBody (self)
+   self:_trace (' ...init body started')
+   while true do
+      self:_trace (' ...init yielding')
+      local evt = coroutine.yield ()
+      self:_push (evt or true) -- push sentinel if there is no event
+      if evt then
+         self:_broadcast (evt)
+      end
+      self:_trace (' ...outer cycle started: %s', evt)
+      while #self.stack > 0 do
+         self:_trace (' ...innter cycle started: %s',
+                      list2str (self.stack))
+         local nblocked = 0
+         local i = 1
+         while i <= #self.children do
+            local child = assert (self.children[i])
+            local status = coroutine.status (child.co)
+            if status ~= 'suspended' then -- zombie child
+               assert (status == 'dead')
+               self:_trace (' ...removing: %s', child)
+               bhvRemoveChild (self, i)
+            else
+               self:_trace (' ...advancing: %s', child)
+               local result = child:_resumeSafe ()
                if result == 'awaiting' then
                   i = i + 1
+                  nblocked = nblocked + 1
                elseif result == 'emitting' then
                   break
-               elseif result == nil then
-                  self:_trace ('... removing trail: %s', child)
-                  assert (coroutine.status (child.co) == 'dead')
-                  bhvRemoveChild (self, i)
-               else
-                  error'should not get here'
                end
             end
-            if not progress then
-               self:_trace ('... popping')
-               self:_pop ()
-            end
-            self:_trace ('... internal cycle done')
          end
-         self:_trace ('... external cycle done')
+         if nblocked == #self.children then -- all children are blocked
+            local out = self:_pop ()
+            self:_trace (' ...popping: %s', args2str (out))
+         end
+         self:_trace (' ...inner cycle ended')
       end
+      self:_trace (' ...outer cycle ended')
    end
-   local init = assert (bhvNew (func, name or 'init'))
-   init:_resume (init)          -- bootstrap init
+end
+
+--- Creates an initial behavior.
+-- An initial behavior is the root behavior in a behavior tree.
+-- @tparam[opt] table opts Options.
+function behavior.init (opts)
+   local opts = opts or {}
+   --
+   local name = opts.name or 'init'
+   local error = opts.error or bhvError
+   local trace = opts.trace or false
+   --
+   local init = assert (bhvNew (bhvInitBody, name))
+   --
+   init.error = error
+   init.trace = trace
+   init:_trace ('init %s', tab2str (opts))
+   init:_resume (init)          -- bootstrap
    return init
 end
 
+--- Spawns behaviors.
+-- @tparam table t List of functions to be used as behavior bodies.
 function behavior:spawn (t)
    self:_trace ('spawn (#%d)', #t)
    if type (t) == 'function' then
       t = {t}
    end
+   local curr = self:_getBehavior ()
+   local parent = curr or self.root
+   local root = parent.root
    assert (type (t) == 'table')
    for i,f in ipairs (t) do
-      local child = bhvNew (f, self.name..':'..i, self.root)
-      self.root:_trace ('... spawing trail: %s', child)
+      local child = assert (bhvNew (f, parent.name..'.spawn'..i, root))
+      self.root:_trace (' ...adding: %s', child)
    end
-   if self:_getBehavior () == nil then -- if not called by a behavior,
-      self.root:_resume ()             --   bootstrap the spawn behaviors
+   if not curr then             -- if not called by a behavior,
+      self.root:_resume ()      --   bootstrap the spawn behaviors
    end
 end
 
+--- Awaits for event matching condition.
+-- This function does nothing if it is not called by a behavior.
+-- @param cond The condition to be awaited.
 function behavior:await (cond)
    local curr = self:_getBehavior ()
    if curr == nil then
       return                    -- not called by a behavior, do nothing
    end
    curr:_trace ('await (%s)', cond)
-   curr.awaiting = cond
+   curr.awaiting = true
    local res
    repeat
-      curr:_trace ('... await yielding')
+      curr:_trace (' ...await yielding: awaiting')
       res = coroutine.yield ('awaiting')
    until res == cond
-   curr:_trace ('... await consumed')
-   curr.awaiting = nil
-   coroutine.yield ()           -- ready to continue
+   curr:_trace (' ...await consumed')
+   curr.awaiting = false
+   coroutine.yield ()           -- ready to advance
 end
 
+--- Emits event.
+-- @param evt the event to be emitted.
 function behavior:emit (evt)
    local curr = self:_getBehavior ()
    if curr == nil then          -- external call
-      self:_trace ('emit external (%s)', evt)
+      self:_trace ('emit (%s) <<external>>', evt)
+      self:_trace (' ...pushing: %s', args2str (evt))
       self.root:_push (evt)
       self.root:_broadcast (evt)
       self.root:_resume ()
    else                         -- internal call
-      curr:_trace ('emit internal (%s)', evt)
+      curr:_trace ('emit (%s) <<internal>>', evt)
+      curr:_trace (' ...pushing: %s', args2str (evt))
       curr.canrun = #curr.root.stack
       curr.root:_push (evt)
-      curr.emitting = evt
+      curr.emitting = true
       curr.root:_broadcast (evt)
-      curr:_trace ('... emit yielding')
+      curr:_trace (' ...emit yielding: emitting')
       coroutine.yield ('emitting') -- start new cycle
       while curr.canrun ~= #curr.root.stack do
-         curr:_trace ('... emit yielding')
+         curr:_trace (' ...emit yielding: awaiting')
          coroutine.yield ('awaiting') -- awaiting for stack level
       end
-      curr:_trace ('... emit consumed')
+      curr:_trace (' ...emit consumed')
+      curr.emitting = false
       curr.canrun = nil
-      curr.emitting = nil
    end
+end
+
+local function bhvParBody (par)
+   par:_trace (' ...par/%s started', par.kind)
+   while true do
+      local result
+      local i = 1
+      while i <= #par.children do
+         local child = assert (par.children[i])
+         par:_trace (' ...advancing: %s', child)
+         result = child:_resumeSafe ()
+         par:_trace ('... advanced to: %s', result)
+         if result == 'awaiting' then -- child blocked on await
+            i = i + 1
+         elseif result == 'emitting' then -- child executed an emit
+            break
+         elseif result == nil then
+            if coroutine.status (child.co) == 'suspended' then
+               goto continue    -- keep advancing child
+            end
+            assert (coroutine.status (child.co) == 'dead') -- child ended
+            if par.kind == 'and' then
+               par:_trace (' ...removing: %s', child)
+               bhvRemoveChild (par, i)
+               if #par.children == 0 then
+                  goto done
+               end
+            elseif par.kind == 'or' then
+               while #par.children > 0 do
+                  par:_trace (' ...removing: %s', par.children[1])
+                  child = bhvRemoveChild (par, 1)
+               end
+               goto done
+            else
+               error'should not get here'
+            end
+         else
+            error'should not get here'
+         end
+         ::continue::
+      end
+      coroutine.yield (result)
+   end
+   ::done::
+   par:_trace (' ...par/%s ended', par.kind)
+   return
+end
+
+function behavior:_par (kind, t)
+   assert (kind == 'or' or kind == 'and')
+   assert (type (t) == 'table')
+   if #t == 0 then
+      return                    -- no functions, do nothing
+   end
+   local curr = self:_getBehavior ()
+   local parent = curr or self
+   local par = assert (bhvNew (bhvParBody, parent.name..'.'..kind, parent))
+   par.kind = kind
+   if not curr then
+      self:_trace ('par/%s (#%d) <<external>>', par.kind, #t)
+   else
+      self:_trace ('par/%s (#%d) <<internal>>', par.kind, #t)
+   end
+   for i,f in ipairs (t) do
+      local child = assert (bhvNew (f, par.name..'.'..i, par))
+      self:_trace (' ...adding: %s', child)
+   end
+   if not curr then             -- external call
+      par:_resume (par)
+   else                         -- internal call
+      while coroutine.status (par.co) ~= 'dead' do
+         self:_trace (' ...advancing: %s', par)
+         coroutine.yield (par:_resume (par))
+      end
+   end
+end
+
+--- Runs behaviors in a par/or composition.
+-- The par/and composition executes until *any* of its behaviors terminates.
+-- @tparam table t List of functions to be used as behavior bodies.
+function behavior:parOr (t)
+   return self:_par ('or', t)
+end
+
+--- Runs behaviors in a par/and composition.
+-- The par/and composition executes until *all* its behaviors terminate.
+-- @tparam table t List of functions to be used as behavior bodies.
+function behavior:parAnd (t)
+   return self:_par ('and', t)
 end
 
 return behavior
--- tests/test-behavior.lua
+++ tests/test-behavior.lua
@@ -4,6 +4,7 @@ local error     = error
 local ipairs    = ipairs
 local pairs     = pairs
 local print     = print
+local type      = type
 
 local behavior = require'behavior'
 _ENV = nil
@@ -69,6 +70,7 @@ do
          t[#t+1] = 'w'
       end,
    }
+   --bhv:_dump ()
    assert (#t == 5)
    assert (t[1] == 'x')
    assert (t[2] == 'y')
@@ -81,12 +83,19 @@ do
    local bhv = behavior.init ()
    -- bhv.trace = true
    local t = {}
-   bhv.error = print
+   local called = false
+   bhv.error = function (obj, errmsg)
+      assert (obj)
+      assert (errmsg)
+      assert (type (errmsg) == 'string')
+      called = true
+   end
    bhv:spawn {
       function ()
          error'argh'
       end
    }
+   assert (called)
 end
 
 -- await/emit --------------------------------------------------------------
@@ -126,4 +135,199 @@ do
    assert (t[1] == 'y')
    assert (t[2] == 'x')
    assert (t[3] == 'z')
+end
+
+-- par ---------------------------------------------------------------------
+
+do
+   local bhv = behavior.init ()
+   local t = {}
+   bhv:parOr {                  -- par/or outside behavior
+      function ()
+         t[#t+1] = 'x'
+      end,
+      function ()
+         t[#t+1] = 'y'
+      end,
+   }
+   assert (#t == 1)
+   assert (t[1] == 'x')
+
+   t = {}
+   bhv:parAnd {                 -- par/and outside behavior
+      function ()
+         t[#t+1] = 'x'
+      end,
+      function ()
+         t[#t+1] = 'y'
+      end,
+   }
+   assert (#t == 2)
+   assert (t[1] == 'x')
+   assert (t[2] == 'y')
+
+   t = {}
+   bhv:spawn {
+      function ()
+         t[#t+1] = 'x'
+         bhv:parOr {            -- par/or inside behavior
+            function ()
+               t[#t+1] = 'y'
+            end,
+            function ()
+               t[#t+1] = 'z'
+            end
+         }
+         t[#t+1] = 'w'
+      end,
+   }
+   assert (#t == 3)
+   assert (t[1] == 'x')
+   assert (t[2] == 'y')
+   assert (t[3] == 'w')
+
+   t = {}
+   bhv:spawn {
+      function ()
+         t[#t+1] = 'x'
+         bhv:parAnd {           -- par/and inside behavior
+            function ()
+               t[#t+1] = 'y'
+            end,
+            function ()
+               t[#t+1] = 'z'
+            end
+         }
+         t[#t+1] = 'w'
+      end,
+   }
+   assert (#t == 4)
+   assert (t[1] == 'x')
+   assert (t[2] == 'y')
+   assert (t[3] == 'z')
+   assert (t[4] == 'w')
+end
+
+do
+   local bhv = behavior.init {}
+   local t = {}
+
+   bhv:parOr {                  -- par/or outside behavior,
+      function ()               --   awaiting external emit
+         t[#t+1] = 'x'
+         bhv:await'A'
+         t[#t+1] = 'y'
+      end,
+      function ()
+         t[#t+1] = 'z'
+         bhv:await'A'
+         t[#t+1] = 'w'
+      end
+   }
+   assert (#t == 2)
+   assert (t[1] == 'x')
+   assert (t[2] == 'z')
+
+   bhv:spawn {
+      function ()
+         t[#t+1] = '#'
+      end
+   }
+   assert (#t == 3)
+   assert (t[3] == '#')
+
+   bhv:emit'A'
+   assert (#t == 4)
+   assert (t[4] == 'y')
+
+   t = {}
+   bhv:parAnd {                 -- par/and outside behavior,
+      function ()               --   awaiting external emit
+         t[#t+1] = 'x'
+         bhv:await'A'
+         t[#t+1] = 'y'
+      end,
+      function ()
+         t[#t+1] = 'z'
+         bhv:await'A'
+         t[#t+1] = 'w'
+      end
+   }
+   assert (#t == 2)
+   assert (t[1] == 'x')
+   assert (t[2] == 'z')
+
+   bhv:spawn {
+      function ()
+         t[#t+1] = '#'
+      end
+   }
+   assert (#t == 3)
+   assert (t[3] == '#')
+
+   bhv:emit'A'
+   assert (#t == 5)
+   assert (t[4] == 'y')
+   assert (t[5] == 'w')
+end
+
+do
+   local bhv = behavior.init {trace=false}
+   local t = {}
+   bhv:spawn {
+      function ()
+         t[#t+1] = 'spawn:1'
+         bhv:await'A'
+         t[#t+1] = 'spawn:2'
+         bhv:parAnd {
+            function ()
+               t[#t+1] = 'and1:1'
+               bhv:await'B'
+               t[#t+1] = 'and1:2'
+               bhv:parOr {
+                  function ()
+                     t[#t+1] = 'or1:1'
+                     bhv:emit'A'
+                     t[#t+1] = 'or1:2'
+                  end,
+                  function ()
+                     t[#t+1] = 'or2:1'
+                  end,
+               }
+               t[#t+1] = 'and1:3'
+            end,
+            function ()
+               t[#t+1] = 'and2:1'
+               bhv:await'B'
+               t[#t+1] = 'and2:2'
+            end,
+            function ()
+               t[#t+1] = 'and3:1'
+               bhv:emit'B'
+               t[#t+1] = 'and3:2'
+               bhv:await'A'
+               t[#t+1] = 'and3:3'
+            end
+         }
+         t[#t+1] = 'spawn:3'
+      end,
+   }
+   assert (#t == 1)
+   assert (t[1] == 'spawn:1')
+
+   bhv:emit'A'
+   assert (t[2] == 'spawn:2')
+   assert (t[3] == 'and1:1')
+   assert (t[4] == 'and2:1')
+   assert (t[5] == 'and3:1')
+   assert (t[6] == 'and1:2')
+   assert (t[7] == 'or1:1')
+   assert (t[8] == 'or2:1')
+   assert (t[9] == 'and1:3')
+   assert (t[10] == 'and2:2')
+   assert (t[11] == 'and3:2')
+
+   bhv:emit'A'
+   assert (t[12] == 'and3:3')
+   assert (t[13] == 'spawn:3')
 end
